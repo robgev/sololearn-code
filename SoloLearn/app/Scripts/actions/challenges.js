bb505b9a6f00f04b7900f1ac663205d4ecbbaef5
Robert Gevorgyan <robert1999.g@gmail.com>
2017-11-06 21:46:08 +0400

    Fix lint errors in challenges.js

--- SoloLearn/app/Scripts/actions/challenges.js
+++ SoloLearn/app/Scripts/actions/challenges.js
@@ -1,6 +1,6 @@
+import { browserHistory } from 'react-router';
 import Service from '../api/service';
 import * as types from '../constants/ActionTypes';
-import { browserHistory } from 'react-router';
 
 import contestTypes from '../defaults/contestTypes';
 
@@ -9,67 +9,80 @@ export const getContests = contests => ({
 	payload: contests,
 });
 
-export const getContestsInternal = () => dispatch => Service.request('Challenge/GetContestFeed').then((response) => {
-	const contestsFeed = response.feed;
+export const getContestsInternal = () => dispatch =>
+	Service.request('Challenge/GetContestFeed')
+		.then((response) => {
+			const contestsFeed = response.feed;
 
-	const completedChallenges = contestsFeed.filter(item => item.player.status == contestTypes.Won || item.player.status == contestTypes.Lost ||
-                        item.player.status == contestTypes.Draw || item.player.status == contestTypes.Expired ||
-                        item.player.status == contestTypes.GotDeclined);
+			const completedChallenges = contestsFeed.filter(item =>
+				item.player.status === contestTypes.Won ||
+					item.player.status === contestTypes.Lost ||
+					item.player.status === contestTypes.Draw ||
+					item.player.status === contestTypes.Expired ||
+					item.player.status === contestTypes.GotDeclined);
 
-	const ongoingChallenges = contestsFeed.filter(item => item.player.status == contestTypes.Started ||
-                                                                    item.player.status == contestTypes.Challenged);
+			const ongoingChallenges = contestsFeed.filter(item =>
+				item.player.status === contestTypes.Started ||
+					item.player.status === contestTypes.Challenged);
 
-	const invitedChallenges = contestsFeed.filter(item => item.player.status == contestTypes.GotChallenged);
+			const invitedChallenges = contestsFeed.filter(item =>
+				item.player.status === contestTypes.GotChallenged);
 
-	const structurizedContests = {
-		invited: invitedChallenges,
-		ongoing: ongoingChallenges,
-		completed: completedChallenges,
-	};
+			const structurizedContests = {
+				invited: invitedChallenges,
+				ongoing: ongoingChallenges,
+				completed: completedChallenges,
+			};
 
-	dispatch(getContests(structurizedContests));
-}).catch((error) => {
-	console.log(error);
-});
+			dispatch(getContests(structurizedContests));
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 
-export const clearContests = () => dispatch => new Promise((resolve, reject) => {
-	dispatch({ type: types.CLEAR_CONTESTS });
-	resolve();
-});
-
-export const clearContestsInternal = () => dispatch => Service.request('Challenge/ClearContestResults')
-	.then(() => dispatch(clearContests()))
-	.catch((error) => {
-		console.log(error);
+export const clearContests = () => dispatch =>
+	new Promise((resolve) => {
+		dispatch({ type: types.CLEAR_CONTESTS });
+		resolve();
 	});
 
-export const chooseContestCourse = courseId => dispatch => new Promise((resolve, reject) => {
-	dispatch({
-		type: types.CHOOSE_CHALLENGE_COURSE,
-		payload: courseId,
+export const clearContestsInternal = () => dispatch =>
+	Service.request('Challenge/ClearContestResults')
+		.then(() => dispatch(clearContests()))
+		.catch((error) => {
+			console.log(error);
+		});
+
+export const chooseContestCourse = courseId => dispatch =>
+	new Promise((resolve) => {
+		dispatch({
+			type: types.CHOOSE_CHALLENGE_COURSE,
+			payload: courseId,
+		});
+		resolve();
 	});
-	resolve();
-});
 
 export const getAllPlayers = players => ({
 	type: types.GET_ALL_PLAYERS,
 	payload: players,
 });
 
-export const getAllPlayersInternal = (name, courseId) => dispatch => Service.request('Challenge/FindPlayers', { name, courseId })
-	.then((response) => {
-		dispatch(getAllPlayers(response.users));
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+export const getAllPlayersInternal = (name, courseId) => dispatch =>
+	Service.request('Challenge/FindPlayers', { name, courseId })
+		.then((response) => {
+			dispatch(getAllPlayers(response.users));
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 
-export const emptyAllPlayers = () => dispatch => new Promise((resolve, reject) => {
-	dispatch({
-		type: types.EMPTY_ALL_PLAYERS,
+export const emptyAllPlayers = () => dispatch =>
+	new Promise((resolve) => {
+		dispatch({
+			type: types.EMPTY_ALL_PLAYERS,
+		});
+		resolve();
 	});
-	resolve();
-});
 
 export const getContest = contest => ({
 	type: types.GET_CONTEST,
@@ -82,7 +95,7 @@ export const createContestInternal = opponentId => (dispatch, getState) => {
 
 	return Service.request('Challenge/CreateContest', { courseId, opponentId })
 		.then((response) => {
-			const contest = response.contest;
+			const { contest } = response;
 			dispatch(getContest(contest));
 			browserHistory.push(`/challenge/${contest.id}`);
 		})
@@ -91,10 +104,11 @@ export const createContestInternal = opponentId => (dispatch, getState) => {
 		});
 };
 
-export const getContestInternal = id => dispatch => Service.request('Challenge/GetContest', { id })
-	.then((response) => {
-		dispatch(getContest(response.contest));
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+export const getContestInternal = id => dispatch =>
+	Service.request('Challenge/GetContest', { id })
+		.then((response) => {
+			dispatch(getContest(response.contest));
+		})
+		.catch((error) => {
+			console.log(error);
+		});
