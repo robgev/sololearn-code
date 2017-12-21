971fcd1052cb6b5840dd8b67c21f6cdade43742e
Robert Gevorgyan <robert1999.g@gmail.com>
2017-12-21 18:59:05 +0400

    Add following logic in feed sugestions

--- SoloLearn/app/Scripts/containers/Feed/FeedSuggestion.js
+++ SoloLearn/app/Scripts/containers/Feed/FeedSuggestion.js
@@ -1,12 +1,11 @@
 // React modules
 import React, { Component } from 'react';
-
-// Material UI components
+import { connect } from 'react-redux';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import { blueGrey900 } from 'material-ui/styles/colors';
-
 import ProfileAvatar from 'components/Shared/ProfileAvatar';
+import { followUserInternal, unfollowUserInternal } from 'actions/profile';
 
 const styles = {
 	user: {
@@ -53,15 +52,31 @@ const styles = {
 	},
 };
 
-class FeedSuggestion extends Component {
-	constructor(props) {
-		super(props);
+const mapDispatchToProps = {
+	followUser: followUserInternal,
+	unfollowUser: unfollowUserInternal,
+};
 
-		this.state = {
-		};
+@connect(null, mapDispatchToProps)
+class FeedSuggestion extends Component {
+	state = {
+		following: false,
+	}
+
+	handleFollow = () => {
+		const { suggestion: { id }, followUser, unfollowUser } = this.props;
+		const newFollowingValue = !this.state.following;
+		this.setState({ following: newFollowingValue });
+
+		if (newFollowingValue) {
+			followUser(id, null);
+		} else {
+			unfollowUser(id, null);
+		}
 	}
 
 	render() {
+		const { following } = this.state;
 		const { suggestion } = this.props;
 
 		return (
@@ -76,11 +91,13 @@ class FeedSuggestion extends Component {
 				/>
 				<p style={styles.followers}>Followers {suggestion.followers}</p>
 				<RaisedButton
-					label="Follow"
 					labelColor="#fff"
+					secondary={following}
+					onClick={this.handleFollow}
 					backgroundColor={blueGrey900}
 					style={styles.followButton.base}
 					labelStyle={styles.followButton.label}
+					label={following ? 'Following' : 'Follow'}
 					buttonStyle={styles.followButton.button}
 				/>
 			</Paper>
