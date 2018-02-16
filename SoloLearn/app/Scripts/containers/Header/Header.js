b12f2e31ecdb833ae082e70729d1d4273b7e3980
Robert Gevorgyan <robert1999.g@gmail.com>
2018-02-16 22:29:24 +0400

    Add search icon with animations

--- SoloLearn/app/Scripts/containers/Header/Header.js
+++ SoloLearn/app/Scripts/containers/Header/Header.js
@@ -3,20 +3,26 @@ import React from 'react';
 import { Link } from 'react-router';
 import 'styles/header.scss';
 import HomeIcon from './HomeIcon';
+import Search from './HeaderSearch';
 
 // Additional components
 import TabList from './Tabs';
 import NotificationManager from '../Notifications/NotificationManager';
 import SettingsMenu from './HeaderSettingsMenu';
 
+// /codes/something/something splitted by / returns ["", "codes", ....] so we take
+// the element with the index 1 => current section.
+const getCurrentSection = pathName => pathName.split('/')[1];
+
 const Header = ({ pathname }) => (
 	<div className="header">
+		{console.log(pathname)}
 		<div className="header-wrapper">
 			<div className="header-right">
 				<Link to="/feed" className="home-icon">
 					<HomeIcon />
 				</Link>
-				{/* Search */}
+				<Search currentSection={getCurrentSection(pathname)} />
 				<TabList pathname={pathname} />
 			</div>
 			<div className="header-left">
---
+++ SoloLearn/app/Scripts/containers/Header/HeaderSearch.js
@@ -0,0 +1,92 @@
+// React modules
+import React, { PureComponent } from 'react';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import AutoComplete from 'material-ui/AutoComplete';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import SearchIcon from 'material-ui/svg-icons/action/search';
+import CloseIcon from 'material-ui/svg-icons/navigation/close';
+
+class HeaderSearch extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchValue: '',
+			searchOpened: false,
+
+			// /codes/something/something splitted by / returns ["", "codes", ....] so we take
+			// the element with the index 1 => current section.
+			searchArea: props.currentSection,
+		};
+	}
+
+	toggleSearch = () => {
+		const { searchOpened } = this.state;
+		this.setState({ searchOpened: !searchOpened });
+	}
+
+	handleMenuChange = (e, idx, searchArea) => {
+		this.setState({ searchArea });
+	}
+
+	handleSearchTextChange = (e) => {
+		this.setState({ searchValue: e.target.value });
+	}
+
+	handleKeyPress = (e) => {
+		if (e.key === 'Enter') {
+			// Do something.
+		}
+	}
+
+	render() {
+		const { searchValue, searchOpened, searchArea } = this.state;
+		return (
+			<div className="header-search-container">
+				<IconButton
+					onClick={this.toggleSearch}
+					className="header-search-icon"
+					iconStyle={{
+						width: 27,
+						height: 27,
+						opacity: searchOpened ? 0 : 1,
+						transition: 'opacity 200ms cubic-bezier(0.4, 0.0, 0.2, 1)',
+					}}
+				>
+					<SearchIcon color="white" />
+				</IconButton>
+				<div className={`header-overlay ${searchOpened ? 'open' : ''}`}>
+					<DropDownMenu
+						value={searchArea}
+						style={{ height: 56 }}
+						labelStyle={{ color: 'white' }}
+						className="header-dropdown"
+						underlineStyle={{ border: 'none' }}
+						onChange={this.handleMenuChange}
+					>
+						<MenuItem value="codes" primaryText="Codes" />
+						<MenuItem value="discuss" primaryText="Posts" />
+						<MenuItem value="users" primaryText="Users" />
+						<MenuItem value="learn" primaryText="Lessons" />
+					</DropDownMenu>
+					<div className="search-wrapper">
+						<AutoComplete
+							fullWidth
+							dataSource={[]}
+							hintText="Search"
+							underlineShow={false}
+							searchText={searchValue}
+							onUpdateInput={this.onChange}
+							onKeyPress={this.handleKeyPress}
+						/>
+					</div>
+					<IconButton onClick={this.toggleSearch}>
+						<CloseIcon color="white" />
+					</IconButton>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default HeaderSearch;
