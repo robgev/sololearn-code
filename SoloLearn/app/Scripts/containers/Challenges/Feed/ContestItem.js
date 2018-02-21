efa3678a2d4adb221d9e4b09f6ae1265a151d83a
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-02-21 22:30:47 +0400

    update expire time in challenges feed

--- SoloLearn/app/Scripts/containers/Challenges/Feed/ContestItem.js
+++ SoloLearn/app/Scripts/containers/Challenges/Feed/ContestItem.js
@@ -11,6 +11,9 @@ import { updateDate, getChallengeStatus } from 'utils';
 import contestTypes from 'defaults/contestTypes';
 import ProfileAvatar from 'components/Shared/ProfileAvatar';
 
+// i18n
+import { translate } from 'react-i18next';
+
 const styles = {
 	content: {
 		display: 'flex',
@@ -64,7 +67,7 @@ const styles = {
 		fontSize: '11px',
 	},
 };
-
+@translate()
 class ContestItemBase extends PureComponent {
 	setContest = () => {
 		const { contestId } = this.props;
@@ -72,14 +75,15 @@ class ContestItemBase extends PureComponent {
 	}
 
 	getDateDifference = (expireDate) => {
+		const { t } = this.props;
 		const dateNow = moment();
 		const expiryDate = moment.utc(expireDate);
 		const duration = moment.duration(expiryDate.diff(dateNow));
 		const hours = Math.floor(duration.asHours());
 		const minutes = Math.floor(duration.asMinutes());
 		return hours < 1 ?
-			`Expires in ${minutes} min` :
-			`Expires in ${hours} hr`;
+			`${t('play.challenge-expire-format')} ${minutes} ${t('date.min')}` :
+			`${t('play.challenge-expire-format')} ${hours} ${t('date.hour')}`;
 	}
 
 	render() {
