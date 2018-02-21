ae22a309d3229c977b2be342097b748b7831a7a0
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-02-21 16:05:45 +0400

    add localization to default/texts

--- SoloLearn/app/Scripts/defaults/texts.js
+++ SoloLearn/app/Scripts/defaults/texts.js
@@ -1,22 +1,39 @@
+import i18n from '../../i18n';
 // App texts
 
 const texts = {
 	// Popups
 	// popupYes: 'Yes',
 	// popupNo: 'No',
-	popupOk: 'OK',
-	popupCancel: 'Cancel',
+	get popupOk() {
+		return i18n.t('common.ok-title');
+	},
+	get popupCancel() {
+		return i18n.t('common.cancel-title');
+	},
 	// popupRetry: 'Retry',
-	popupDelete: 'Delete',
+	get popupDelete(){
+		return i18n.t('common.delete-title');
+	},
 
 	// Hints
-	hintHintConfirmText: 'Use {placeholder} points to get a hint',
-	hintHintConfirmApply: 'OK',
-	hintSkipConfirmText: 'Use {placeholder} points to unlock the correct answer',
-	hintSkipConfirmApply: 'Unlock',
-	hintNoEnoughPoints: 'You don\'t have enough points to use this feature.',
+	get hintHintConfirmText() {
+		return i18n.t('learn.get-hint-format');
+	},
+	get hintHintConfirmApply() {
+		return i18n.t('common.ok-title');
+	},
+	get hintSkipConfirmText() {
+		return i18n.t('learn.popups.unlock.description');
+	},
+	get hintSkipConfirmApply() {
+		return i18n.t('learn.popups.unlock-popup-ok-action-title');
+	},
+	get hintNoEnoughPoints() {
+		return i18n.t('learn.hint-not-enough-xp');
+	},
 	// hintSkipTip: 'Unlock the answer',
-	hintHintTip: 'Get a hint',
+	// hintHintTip: 'Get a hint',
 
 	// Congrats
 	// congratsTitle: 'Congratulations',
@@ -35,18 +52,30 @@ const texts = {
 
 	// Profile
 	// resetProgressConfirmText: 'All your progress on this course will be lost. Are you sure you want to continue?',
-	resetContinue: 'RESET',
+	get resetContinue() {
+		return i18n.t('learn.reset-course-popup-title');
+	},
 
 	// Shortcut
-	shortcutButton: 'TAKE A SHORTCUT',
+	get shortcutButton() {
+		return i18n.t('learn.take-shortcut');
+	},
 	// closeShortcut: 'Are you sure you want to leave the test?',
-	shortcutFailed: 'Test out failed. You ran out of lives.',
-	shortcutSucceed: 'You have successfully completed the test!',
-	shortcutContinue: 'CONTINUE',
+	get shortcutFailed() {
+		return i18n.t('learn.lesson-test-failed-message');
+	},
+	get shortcutSucceed() {
+		return i18n.t('learn.test-completed-message');
+	},
+	get shortcutContinue() {
+		return i18n.t('learn.buttons-continue');
+	},
 	// shortcutLeave: 'LEAVE',
 
 	// Comments
-	commentDeleteConfirmText: 'Are you sure you want to delete the comment?',
+	get commentDeleteConfirmText() {
+		return i18n.t('comments.delete-comment-popup-message');
+	},
 
 	// Playground
 	// c_cpp: '#include <iostream>\nusing namespace std;\n\nint main() {\n\treturn 0;\n}',
@@ -61,41 +90,88 @@ const texts = {
 	// csharp: 'using System;\nusing System.Collections.Generic;\nusing System.Linq;\nusing System.Text;\nusing System.Threading.Tasks;\n\n\nnamespace SoloLearn\n{\n\tclass Program\n\t{\n\t\tstatic void Main(string[] args)\n\t\t{\n\t\t\t\n\t\t}\n\t}\n}\n',
 	// ruby: '',
 
-	inputsPopupTitle: 'Looks like your program needs input',
+	get inputsPopupTitle() {
+		return i18n.t('code_playground.alert.input-needs-title');
+	},
 	// inputsPopupSubTitle: 'Split multiple inputs into separate lines',
-	savePopupTitle: 'Save Code',
-	savePopupSubTitle: 'Code Name:',
-	codeNameError: 'Code name cannot be empty',
+	get savePopupTitle() {
+		return i18n.t('code_playground.popups.save-popup-title');
+	},
+	get savePopupSubTitle() {
+		return i18n.t('code_playground.popups.save-popup-code-name-placeholder');
+	},
+	get codeNameError() {
+		return i18n.t('code_playground.alert.invalid-name-title');
+	},
 	externalSourcePopupTitle: 'External Resources',
 	externalSourcePopupSubTitle: 'Coose source:',
 
 	// Discuss
-	postDeleteConfirmText: 'Are you sure you want to delete this post?',
-
+	get postDeleteConfirmText() {
+		return i18n.t('discuss.delete-question-message');
+	},
 	// Settings
-	challengesBanner: 'Accept challenges using these weapons:',
-	customizeFeed: 'Customize Your Feed',
+	get challengesBanner() {
+		return i18n.t('weapons.section-header-title');
+	},
+	get customizeFeed() {
+		return i18n.t('notification_settings.feed-section-title');
+	},
 	feedItemNames: {
-		feedAchievements: 'Profile Updates',
-		feedPostQuestions: 'Questions',
-		feedPostAnswers: 'Answers',
-		feedCodes: 'Codes',
-		feedCourses: 'Courses',
-		feedLessons: 'Lessons',
-		feedChallenges: 'Challenges',
-		feedLessonComments: 'Lesson Comments',
-		feedCodeComments: 'Code Comments',
+		get feedAchievements() {
+			return i18n.t('notification_settings.feed-profile-updates');
+		},
+		get feedPostQuestions() {
+			return i18n.t('notification_settings.feed-questions');
+		},
+		get feedPostAnswers() {
+			return i18n.t('notification_settings.feed-answers');
+		},
+		get feedCodes() {
+			return i18n.t('notification_settings.feed-codes');
+		},
+		get feedCourses() {
+			return i18n.t('notification_settings.feed-courses');
+		},
+		get feedLessons() {
+			return i18n.t('notification_settings.feed-lessons');
+		},
+		get feedChallenges() {
+			return i18n.t('notification_settings.feed-challenges');
+		},
+		get feedLessonComments() {
+			return i18n.t('notification_settings.feed-lesson-comments');
+		},
+		get feedCodeComments() {
+			return i18n.t('notification_settings.feed-code-comments');
+		},
 	},
 
 	// Leaderboards
-	today: 'Today',
-	thisWeek: 'This Week',
-	thisMonth: 'This Month',
-	allTime: 'All Time',
-	following: 'Following',
-	local: 'Local',
-	global: 'Global',
-	next: '... You are the next',
+	get today() {
+		return i18n.t('leaderboard.picker.day-title')
+	},
+	get thisWeektoday() {
+		return i18n.t('leaderboard.picker.week-title')
+	},
+	get thisMonth() {
+		return i18n.t('leaderboard.picker.month-title')
+	},
+	get allTime() {
+		return i18n.t('leaderboard.picker.global-title')
+	},
+	get following() {
+		return i18n.t('leaderboard.tab.following-title')
+	},
+	get local() {
+		return i18n.t('leaderboard.tab.local-title');
+	},
+	get global() {
+		return i18n.t('leaderboard.tab.global-title');
+	},
+	get next() {
+		return i18n.t('leaderboard.header.own-title');
+	},
 };
 
 export default texts;
