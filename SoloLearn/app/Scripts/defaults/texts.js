8f087d1dfe9ccc8539888f2e3a74d635b091c563
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-02-21 13:10:57 +0400

    comment out useless texts

--- SoloLearn/app/Scripts/defaults/texts.js
+++ SoloLearn/app/Scripts/defaults/texts.js
@@ -2,11 +2,11 @@
 
 const texts = {
 	// Popups
-	popupYes: 'Yes',
-	popupNo: 'No',
+	// popupYes: 'Yes',
+	// popupNo: 'No',
 	popupOk: 'OK',
 	popupCancel: 'Cancel',
-	popupRetry: 'Retry',
+	// popupRetry: 'Retry',
 	popupDelete: 'Delete',
 
 	// Hints
@@ -15,54 +15,54 @@ const texts = {
 	hintSkipConfirmText: 'Use {placeholder} points to unlock the correct answer',
 	hintSkipConfirmApply: 'Unlock',
 	hintNoEnoughPoints: 'You don\'t have enough points to use this feature.',
-	hintSkipTip: 'Unlock the answer',
+	// hintSkipTip: 'Unlock the answer',
 	hintHintTip: 'Get a hint',
 
 	// Congrats
-	congratsTitle: 'Congratulations',
-	congratsPendingTitle: 'Certificate',
-	congratsPoints: 'Points: {placeholder}',
-	congratsContinue: 'Continue Learning',
-	congratsShare: 'Share',
-	congratsPending: 'Complete the course to unlock your certificate',
-	congratsComplete: 'You have successfully completed the <nobr>{placeholder}</nobr> course',
+	// congratsTitle: 'Congratulations',
+	// congratsPendingTitle: 'Certificate',
+	// congratsPoints: 'Points: {placeholder}',
+	// congratsContinue: 'Continue Learning',
+	// congratsShare: 'Share',
+	// congratsPending: 'Complete the course to unlock your certificate',
+	// congratsComplete: 'You have successfully completed the <nobr>{placeholder}</nobr> course',
 
 	// Quiz
-	quizResultWrong: 'Wrong',
-	quizResultCorrect: 'Correct!',
-	quizAttempts: 'Attempt: ',
-	quizResultWrongBackToVideo: 'Back',
+	// quizResultWrong: 'Wrong',
+	// quizResultCorrect: 'Correct!',
+	// quizAttempts: 'Attempt: ',
+	// quizResultWrongBackToVideo: 'Back',
 
 	// Profile
-	resetProgressConfirmText: 'All your progress on this course will be lost. Are you sure you want to continue?',
+	// resetProgressConfirmText: 'All your progress on this course will be lost. Are you sure you want to continue?',
 	resetContinue: 'RESET',
 
 	// Shortcut
 	shortcutButton: 'TAKE A SHORTCUT',
-	closeShortcut: 'Are you sure you want to leave the test?',
+	// closeShortcut: 'Are you sure you want to leave the test?',
 	shortcutFailed: 'Test out failed. You ran out of lives.',
 	shortcutSucceed: 'You have successfully completed the test!',
 	shortcutContinue: 'CONTINUE',
-	shortcutLeave: 'LEAVE',
+	// shortcutLeave: 'LEAVE',
 
 	// Comments
 	commentDeleteConfirmText: 'Are you sure you want to delete the comment?',
 
 	// Playground
-	c_cpp: '#include <iostream>\nusing namespace std;\n\nint main() {\n\treturn 0;\n}',
-	java: 'public class Program\n{\n\tpublic static void main(String[] args) {\n\t\t\n\t}\n}',
-	php: '<?php\n\n?>',
-	html: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>Page Title</title>\n\t</head>\n\t<body>\n\t\t\n\t</body>\n</html>',
-	jqueryHtml: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>Page Title</title>\n\t\t<script src=\"https://code.jquery.com/jquery-3.1.1.js\"></script>\n\t</head>\n\t<body>\n\t\t\n\t</body>\n</html>',
-	jquery: '$(function() {\n\t\n});',
-	css: 'body {\n\t\n}',
-	javascript: '',
-	python: '',
-	csharp: 'using System;\nusing System.Collections.Generic;\nusing System.Linq;\nusing System.Text;\nusing System.Threading.Tasks;\n\n\nnamespace SoloLearn\n{\n\tclass Program\n\t{\n\t\tstatic void Main(string[] args)\n\t\t{\n\t\t\t\n\t\t}\n\t}\n}\n',
-	ruby: '',
+	// c_cpp: '#include <iostream>\nusing namespace std;\n\nint main() {\n\treturn 0;\n}',
+	// java: 'public class Program\n{\n\tpublic static void main(String[] args) {\n\t\t\n\t}\n}',
+	// php: '<?php\n\n?>',
+	// html: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>Page Title</title>\n\t</head>\n\t<body>\n\t\t\n\t</body>\n</html>',
+	// jqueryHtml: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>Page Title</title>\n\t\t<script src=\"https://code.jquery.com/jquery-3.1.1.js\"></script>\n\t</head>\n\t<body>\n\t\t\n\t</body>\n</html>',
+	// jquery: '$(function() {\n\t\n});',
+	// css: 'body {\n\t\n}',
+	// javascript: '',
+	// python: '',
+	// csharp: 'using System;\nusing System.Collections.Generic;\nusing System.Linq;\nusing System.Text;\nusing System.Threading.Tasks;\n\n\nnamespace SoloLearn\n{\n\tclass Program\n\t{\n\t\tstatic void Main(string[] args)\n\t\t{\n\t\t\t\n\t\t}\n\t}\n}\n',
+	// ruby: '',
 
 	inputsPopupTitle: 'Looks like your program needs input',
-	inputsPopupSubTitle: 'Split multiple inputs into separate lines',
+	// inputsPopupSubTitle: 'Split multiple inputs into separate lines',
 	savePopupTitle: 'Save Code',
 	savePopupSubTitle: 'Code Name:',
 	codeNameError: 'Code name cannot be empty',
