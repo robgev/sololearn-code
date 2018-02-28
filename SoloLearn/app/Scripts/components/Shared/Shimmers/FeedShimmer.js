46e2fef49c8cbf0ff7bdb0ff5866bb7a329f22cb
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-02-28 19:12:45 +0400

    reorganize shimmer files

--- SoloLearn/app/Scripts/components/Shared/Shimmers/FeedShimmer.js
+++ SoloLearn/app/Scripts/components/Shared/Shimmers/FeedShimmer.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import 'styles/components/Shared/Shimmers/FeedShimmer.scss';
 
-const FeedShimmer = () => (
-	<div>
-		<div className='feed-shimmer-container'></div>
-	</div>
-)
+const FeedShimmer = () => {
+	const renderContainers = Array(4).fill(0).map((_, i) => (
+		<div
+			key={i}
+			className={
+				i % 2 === 0 ?
+				'feed-shimmer-small-container' :
+				'feed-shimmer-medium-container'
+			}
+		/>
+	));
+	return(
+		<div>
+			<div className='feed-shimmer-title'></div>
+			{renderContainers}
+		</div>
+)}
 
 export default FeedShimmer
