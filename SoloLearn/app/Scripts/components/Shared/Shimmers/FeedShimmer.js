273b5fa3b9f51e0fef08e6bc9d824f9a5f5e1a11
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-03-01 14:30:53 +0400

    make feedshimmer a little bit WET but more explicit

--- SoloLearn/app/Scripts/components/Shared/Shimmers/FeedShimmer.js
+++ SoloLearn/app/Scripts/components/Shared/Shimmers/FeedShimmer.js
@@ -2,16 +2,23 @@ import React from 'react';
 import 'styles/components/Shared/Shimmers/FeedShimmer.scss';
 
 const FeedShimmer = () => {
-	const renderContainers = Array(4).fill(0).map((_, i) => (
-		<div
-			key={i}
-			className={
-				i % 2 === 0 ?
-				'feed-shimmer-small-container' :
-				'feed-shimmer-medium-container'
-			}
-		/>
-	));
+	const renderContainers = Array(4).fill(0).map((_, i) => {
+		if (i % 2 === 0) {
+			return (
+				<div
+					key={i}
+					className='feed-shimmer-small-container'
+				/>
+			)
+		} else {
+			return (
+				<div
+					key={i}
+					className='feed-shimmer-medium-container'
+				/>
+			)
+		}
+});
 	return(
 		<div>
 			<div className='feed-shimmer-title'></div>
