cfcc49fc666954bc350e636bcf5fab6e24abaa25
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-03-10 18:52:04 +0400

    Add finished shimmer to PlayShimmer

--- SoloLearn/app/Scripts/components/Shared/Shimmers/PlayShimmer.js
+++ SoloLearn/app/Scripts/components/Shared/Shimmers/PlayShimmer.js
@@ -2,16 +2,30 @@ import React from 'react';
 import 'styles/components/Shared/Shimmers/PlayShimmer.scss';
 
 const PlayShimmer = () => {
-	const renderContainers = Array(10).fill(0).map((_, i) => {
-		return (
-			<div className='play-shimmer-container' key={i}/>
-		)
-	});
-	return(
-		<div>
-			<div className='play-shimmer-title'></div>
-			{renderContainers}
-		</div>
-)}
+    const renderContainers = Array(20).fill(0).map((_, i) => {
+        return (
+            <div className='play-shimmer-container' style={{marginTop: i * 60}} key={i}>
+              <div className='user'>
+                <div className='avatar'/>
+                <div className='name-and-date'>
+                  <div className='name' />
+                  <div className='date' />
+                </div>
+              </div>
+              <div className='contest'>
+                <div className='language'/>
+                <div className='state'/>
+                <div className='result'/>
+              </div>
+            </div>
+        );
+    });
+    return(
+        <div>
+          {renderContainers}
+          <div className='play-shimmer-shimmer'/>
+        </div>
+    );
+};
 
 export default PlayShimmer
