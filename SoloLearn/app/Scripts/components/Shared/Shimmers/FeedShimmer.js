37c9e5e4d1a63232a7ba551ef9e94ee76cacda69
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-03-10 20:51:49 +0400

    Finish shimmer in Feed

--- SoloLearn/app/Scripts/components/Shared/Shimmers/FeedShimmer.js
+++ SoloLearn/app/Scripts/components/Shared/Shimmers/FeedShimmer.js
@@ -2,28 +2,29 @@ import React from 'react';
 import 'styles/components/Shared/Shimmers/FeedShimmer.scss';
 
 const FeedShimmer = () => {
-	const renderContainers = Array(4).fill(0).map((_, i) => {
-		if (i % 2 === 0) {
-			return (
-				<div
-					key={i}
-					className='feed-shimmer-small-container'
-				/>
-			)
-		} else {
-			return (
-				<div
-					key={i}
-					className='feed-shimmer-medium-container'
-				/>
-			)
-		}
-	});
-	return(
-		<div>
-			<div className='feed-shimmer-title'></div>
-			{renderContainers}
-		</div>
-)}
+    const renderContainers = Array(20).fill(0).map((_, i) => {
+        return (
+            <div className='feed-shimmer-container' style={{marginTop: 20 + i * 170}} key={i}>
+              <div className='post'>
+                <div className='avatar'/>
+                <div className='post-info'>
+                  <div className='poster-name' />
+                  <div className='post' />
+                  <div className='info' />
+                </div>
+              </div>
+              <div className='date-container'>
+                <div className='date' />
+              </div>
+            </div>
+        );
+    });
+    return(
+        <div>
+          {renderContainers}
+          <div className='feed-shimmer-shimmer'/>
+        </div>
+    );
+};
 
 export default FeedShimmer
