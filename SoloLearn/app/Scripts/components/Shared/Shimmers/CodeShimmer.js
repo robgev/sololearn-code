8ce467c845fe03193010858621670b4212557bd8
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-03-10 20:09:51 +0400

    Add finished shimmer to code section

--- SoloLearn/app/Scripts/components/Shared/Shimmers/CodeShimmer.js
+++ SoloLearn/app/Scripts/components/Shared/Shimmers/CodeShimmer.js
@@ -2,15 +2,32 @@ import React from 'react';
 import 'styles/components/Shared/Shimmers/CodeShimmer.scss';
 
 const CodeShimmer = () => {
-	const renderContainers = Array(10).fill(0).map((_, i) => {
-		return (
-			<div className='code-shimmer-container' key={i}/>
-		)
-	});
-	return(
-		<div>
-			{renderContainers}
-		</div>
-)}
+    const renderContainers = Array(10).fill(0).map((_, i) => {
+        return (
+            <div className='code-shimmer-container' style={{marginTop: i * 60}} key={i}>
+              <div className='code'>
+                <div className='avatar' />
+                <div className='code-info'>
+                  <div className='title' />
+                  <div className='info' />
+                </div>
+              </div>
+              <div className='user'>
+                <div className='name-and-date'>
+                  <div className='name' />
+                  <div className='date' />
+                </div>
+                <div className='avatar' />
+              </div>
+            </div>
+        );
+    });
+    return(
+        <div>
+          {renderContainers}
+          <div className='code-shimmer-shimmer'/>
+        </div>
+    );
+};
 
-export default CodeShimmer
+export default CodeShimmer;
