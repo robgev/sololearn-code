498fba734e796a3091c9032aebd40d94237915fb
Robert Gevorgyan <robert1999.g@gmail.com>
2018-03-27 18:54:00 +0400

    Fix drag and drop options overflow bug

--- SoloLearn/app/Scripts/containers/Learn/QuizContainers/PlaceholderDragAndDrop.js
+++ SoloLearn/app/Scripts/containers/Learn/QuizContainers/PlaceholderDragAndDrop.js
@@ -63,7 +63,6 @@ const styles = {
 	bag: {
 		overflow: 'hidden',
 		width: '500px',
-		height: '50px',
 		margin: '15px 0 0 0',
 		boxShadow: 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px',
 	},
