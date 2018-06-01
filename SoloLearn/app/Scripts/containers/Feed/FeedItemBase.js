509bbbfc89943516ea0a059a94bde1b8446d8892
Robert Gevorgyan <robert1999.g@gmail.com>
2018-06-01 16:00:33 +0400

    Add hovers

--- SoloLearn/app/Scripts/containers/Feed/FeedItemBase.js
+++ SoloLearn/app/Scripts/containers/Feed/FeedItemBase.js
@@ -5,75 +5,28 @@ import { determineBadge } from 'utils';
 import ModBadge from 'components/Shared/ModBadge';
 import ProfileAvatar from 'components/Shared/ProfileAvatar';
 
-const styles = {
-	content: {
-		display: 'flex',
-	},
-
-	wrapper: {
-		margin: '0 0 0 5px',
-		flexGrow: 1,
-		width: '90%',
-	},
-
-	title: {
-		fontSize: '14px',
-		color: '#545454',
-		margin: '0 0 7px 0',
-	},
-
-	userName: {
-		fontWeight: 500,
-		color: '#8BC34A',
-		textDecoration: 'none',
-	},
-
-	votes: {
-		margin: '7px 0 0 0',
-	},
-
-	voteIcon: {
-		width: '15px',
-		height: '15px',
-		verticalAlign: 'middle',
-	},
-
-	voteText: {
-		display: 'inline-block',
-		verticalAlign: 'middle',
-		margin: '0 5px',
-		color: '#9e9e9e',
-		fontSize: '13px',
-		width: '25px',
-		textAlign: 'center',
-	},
-
-	linkStyle: {
-		position: 'relative',
-		textDecoration: 'none',
-		zIndex: 2,
-	},
-};
+import 'styles/Feed/FeedItemBase.scss';
 
 const FeedItemBase = ({
 	title, user, children,
 }) => (
-	<div className="content" style={styles.content}>
+	<div className="feed-item-content">
 		<ProfileAvatar
 			size={40}
 			userID={user.id}
 			badge={user.badge}
 			userName={user.name}
-			style={styles.linkStyle}
 			avatarUrl={user.avatarUrl}
 		/>
-		<div className="wrapper" style={styles.wrapper}>
-			<p style={styles.title}>
+		<div className="wrapper">
+			<p className="feed-item-title">
 				<Link
 					to={`/profile/${user.id}`}
-					style={{ ...styles.userName, ...styles.linkStyle }}
+					className="user-name-link"
 				>
-					{`${user.name} `}
+					<span className="user-name-wrapper">
+						{`${user.name} `}
+					</span>
 					<ModBadge
 						className="small"
 						badge={determineBadge(user.badge)}
