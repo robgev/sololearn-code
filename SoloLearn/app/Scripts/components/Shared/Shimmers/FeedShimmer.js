2fb8843c558e0f4a5590b73657f7a9703b20312f
Robert Gevorgyan <robert1999.g@gmail.com>
2018-06-04 13:41:47 +0400

    Change feed shimmer

--- SoloLearn/app/Scripts/components/Shared/Shimmers/FeedShimmer.js
+++ SoloLearn/app/Scripts/components/Shared/Shimmers/FeedShimmer.js
@@ -1,26 +1,40 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
 import 'styles/components/Shared/Shimmers/FeedShimmer.scss';
 
 const FeedShimmer = () => {
 	const renderContainers = Array(20).fill(0).map((_, i) => (
-		<div className="feed-shimmer-container" style={{ marginTop: 20 + (i * 170) }} key={i}>
-			<div className="post">
-				<div className="avatar" />
-				<div className="post-info">
-					<div className="poster-name" />
-					<div className="post" />
-					<div className="info" />
+		<Paper style={{
+			height: 150,
+			padding: 10,
+			marginBottom: 10,
+			position: 'relative',
+			marginTop: i === 0 ? 10 : 0,
+		}}
+		>
+			<div className="feed-shimmer-container" key={i}>
+				<div className="post-head">
+					<div className="avatar" />
+					<div className="post-meta-info">
+						<div className="post-name" />
+						<div className="post-subhead" />
+					</div>
+				</div>
+				<div className="post-body">
+					<div className="post-line medium" />
+					<div className="post-line long" />
+					<div className="post-line short" />
+				</div>
+				<div className="post-footer">
+					<div className="date" />
 				</div>
 			</div>
-			<div className="date-container">
-				<div className="date" />
-			</div>
-		</div>
+			<div className="feed-shimmer-shimmer" />
+		</Paper>
 	));
 	return (
 		<div>
 			{renderContainers}
-			<div className="feed-shimmer-shimmer" />
 		</div>
 	);
 };
