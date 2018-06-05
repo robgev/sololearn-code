574017aef6efb4abecd1bac2729cf8c900a09b83
Robert Gevorgyan <robert1999.g@gmail.com>
2018-06-05 17:58:17 +0400

    Remove react slick

--- SoloLearn/app/Scripts/components/Shared/CollectionCard.js
+++ SoloLearn/app/Scripts/components/Shared/CollectionCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import Slider from 'react-slick';
 import Paper from 'material-ui/Paper';
 import FlatButton from 'material-ui/FlatButton';
 import { translate } from 'react-i18next';
@@ -13,21 +12,6 @@ import CourseChip from './CourseChip';
 const collectionTypes = {
 	slayLessons: 1,
 	courses: 2,
-};
-
-const generateBreakpoints = (collectionItems) => {
-	const breakpointValues = [ 1624, 1224, 768, 320 ];
-	const initialNumberOfShownItems = 5;
-	return breakpointValues.map((currentPoint, index) => {
-		const slidesToShow = initialNumberOfShownItems - (index + 1);
-		return {
-			breakpoint: currentPoint,
-			settings: {
-				slidesToShow,
-				infinite: false,
-			},
-		};
-	});
 };
 
 const CollectionCard = ({
@@ -52,6 +36,7 @@ const CollectionCard = ({
 				padding: 10,
 				width: '100%',
 				marginBottom: 10,
+				overflow: 'hidden',
 			}}
 		>
 			<div className="meta-info">
@@ -65,17 +50,7 @@ const CollectionCard = ({
 					/>
 				}
 			</div>
-			<Slider
-				dots={false}
-				speed={500}
-				swipeToSlide
-				arrows={false}
-				infinite={false}
-				slidesToShow={5}
-				draggable={false}
-				className="courses-list"
-				responsive={generateBreakpoints(collectionItems)}
-			>
+			<div className="courses-list">
 				{
 					collectionItems.map((lessonItem) => {
 						const foundSkill = isCourses
@@ -96,7 +71,7 @@ const CollectionCard = ({
 						);
 					})
 				}
-			</Slider>
+			</div>
 		</Paper>
 	);
 };
