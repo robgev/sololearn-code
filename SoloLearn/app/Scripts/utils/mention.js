162386e33be0265d11fab3416725f3b0401c8383
DavtyanErik <erikdavtyan@gmail.com>
2018-06-05 16:12:18 +0400

    Implement mention utilities

---
+++ SoloLearn/app/Scripts/utils/mention.js
@@ -0,0 +1,36 @@
+import Service from 'api/service';
+
+const mentionTypes = {
+	searchMentionUsers: 'SearchMentionUsers',
+	lessonComment: 'SearchLessonCommentMentionUsers',
+	codeComment: 'SearchCodeCommentMentionUsers',
+	userLessonComment: 'SearchUserLessonCommentMentionUsers',
+	postComment: 'SearchPostCommentMentionUsers',
+};
+
+const getTag = (text, tag) => {
+	const endIndex = text.indexOf(`[/${tag}]`);
+	if (endIndex === -1) return null;
+	const startIndex = text.indexOf(`[${tag}`);
+	const tagged = text.substring(startIndex, endIndex + tag.length + 3);
+	const children = tagged.substring(tagged.indexOf(']') + 1, tagged.indexOf(`[/${tag}`));
+	const attrString = tagged.substring(2 + tag.length, tagged.indexOf(']'));
+	const attrArr = attrString.split(' ');
+	const attributes = {};
+	attrArr.forEach((el) => {
+		const name = el.substring(0, el.indexOf('='));
+		const val = el.substring(el.indexOf('"') + 1, el.length - 1);
+		attributes[name] = val;
+	});
+	return [ text.split(tagged)[0], { attributes, children, tag }, text.split(tagged)[1] ];
+};
+
+export const getTags = (text, tag) => {
+	let resArr = [ text ];
+	while (getTag(resArr[resArr.length - 1], tag) !== null) {
+		resArr = [ ...resArr.slice(0, resArr.length - 1), ...getTag(resArr[resArr.length - 1], tag) ];
+	}
+	return resArr;
+};
+
+export const getMentions = (type, params) => Service.request(mentionTypes[type], params);
