e9eecc58db199bdb6886269812525a14bc248638
Robert Gevorgyan <robert1999.g@gmail.com>
2018-06-11 18:28:01 +0400

    Use numberformatter in slay detailed page

--- SoloLearn/app/Scripts/components/Shared/ViewStats.js
+++ SoloLearn/app/Scripts/components/Shared/ViewStats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { numberFormatter } from 'utils';
 import { grey500 } from 'material-ui/styles/colors';
 import ViewsIcon from 'material-ui/svg-icons/image/remove-red-eye';
 import CommentsIcon from 'material-ui/svg-icons/communication/comment';
@@ -7,11 +8,11 @@ const ViewStats = ({ views, comments, iconStyle }) => (
 	<div className="view-stats">
 		<div className="stat-group">
 			<ViewsIcon color={grey500} style={iconStyle} />
-			<p>{ views }</p>
+			<p>{ numberFormatter(views) }</p>
 		</div>
 		<div className="stat-group">
 			<CommentsIcon color={grey500} style={iconStyle} />
-			<p>{ comments }</p>
+			<p>{ numberFormatter(comments) }</p>
 		</div>
 	</div>
 );
--- SoloLearn/app/Scripts/utils/numberFormatter.js
+++ SoloLearn/app/Scripts/utils/numberFormatter.js
@@ -1,16 +1,15 @@
 const roundToOneDigit = (number) => {
-	const factor = Math.pow(10, 1);
+	const factor = 10 ** 1;
 	return Math.round(number * factor) / factor;
-}
+};
 
 const numberFormatter = (number) => {
 	if (number < 1000) {
 		return `${number}`;
 	} else if (number < 1000000) {
-		return `${roundToOneDigit(number / 1000)}K`
-		
+		return `${roundToOneDigit(number / 1000)}K`;
 	} else if (number < 1000000000) {
-		return `${roundToOneDigit(number / 1000000)}M`
+		return `${roundToOneDigit(number / 1000000)}M`;
 	}
 	return `${roundToOneDigit(number / 1000000000)}B`;
 };
