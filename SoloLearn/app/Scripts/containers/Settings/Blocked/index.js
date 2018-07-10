56e7a5095f8d3a4fcea43366db1a66d7570be2ef
Robert Gevorgyan <robert1999.g@gmail.com>
2018-07-10 16:29:21 +0400

    Add nothing to show info in  blocked users

--- SoloLearn/app/Scripts/containers/Settings/Blocked/index.js
+++ SoloLearn/app/Scripts/containers/Settings/Blocked/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import ReactGA from 'react-ga';
+import { translate } from 'react-i18next';
 import { connect } from 'react-redux';
 import InfiniteScroll from 'react-infinite-scroller';
 import CircularProgress from 'material-ui/CircularProgress';
@@ -15,6 +16,7 @@ const mapDispatchToProps = {
 };
 
 @connect(mapStateToProps, mapDispatchToProps)
+@translate()
 class Blocked extends PureComponent {
 	constructor() {
 		super();
@@ -50,7 +52,7 @@ class Blocked extends PureComponent {
 	}
 
 	render() {
-		const { blockedUsers, blockUser } = this.props;
+		const { t, blockedUsers, blockUser } = this.props;
 		const {
 			loading,
 			hasMore,
@@ -66,28 +68,31 @@ class Blocked extends PureComponent {
 					/>
 				}
 			>
-				<InfiniteScroll
-					pageStart={0}
-					hasMore={hasMore}
-					loadMore={this.loadMore}
-					style={{
-						width: '100%',
-						display: 'flex',
-						flexDirection: 'column',
-					}}
-					loader={loading ? null : <CircularProgress />}
-				>
-					{blockedUsers.map(blockedUser => (
-						<UserCard
-							{...blockedUser}
-							key={blockedUser.id}
-							onBlock={() => blockUser({
-								userId: blockedUser.id,
-								block: !blockedUser.blockedState,
-							})}
-						/>
-					))}
-				</InfiniteScroll>
+				{ !blockedUsers.length ?
+					<p>{t('common.empty-list-message')}</p> :
+					<InfiniteScroll
+						pageStart={0}
+						hasMore={hasMore}
+						loadMore={this.loadMore}
+						style={{
+							width: '100%',
+							display: 'flex',
+							flexDirection: 'column',
+						}}
+						loader={loading ? null : <CircularProgress />}
+					>
+						{blockedUsers.map(blockedUser => (
+							<UserCard
+								{...blockedUser}
+								key={blockedUser.id}
+								onBlock={() => blockUser({
+									userId: blockedUser.id,
+									block: !blockedUser.blockedState,
+								})}
+							/>
+						))}
+					</InfiniteScroll>
+				}
 			</BusyWrapper>
 		);
 	}
