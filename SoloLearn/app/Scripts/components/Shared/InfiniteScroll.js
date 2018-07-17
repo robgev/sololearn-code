0ddf2d8b44a68d61ad573eb87be6b99203c78118
DavtyanErik <erikdavtyan@gmail.com>
2018-07-17 15:32:09 +0400

    Move InfiniteScroll to stateless component

--- SoloLearn/app/Scripts/components/Shared/InfiniteScroll.js
+++ SoloLearn/app/Scripts/components/Shared/InfiniteScroll.js
@@ -3,34 +3,28 @@ import CircularProgress from 'material-ui/CircularProgress';
 import Paper from 'material-ui/Paper';
 import InfiniteScroll from 'react-infinite-scroller';
 
-class InfiniteScrollWrapper extends React.Component {
-	static defaultProps = {
-		element: Paper,
-	}
-	render() {
-		const {
-			children, element: Element, ...rest
-		} = this.props;
-		return (
-			<div>
-				<Element>
-					<InfiniteScroll
-						{...rest}
-						loader={null}
-					>
-						{children}
-					</InfiniteScroll>
-				</Element>
-				{
-					rest.hasMore &&
-					<CircularProgress
-						size={40}
-						style={{ display: 'flex', alignItems: 'center', margin: '10px auto' }}
-					/>
-				}
-			</div>
-		);
-	}
-}
+const InfiniteScrollWrapper = ({ children, element: Element, ...rest }) => (
+	<div>
+		<Element>
+			<InfiniteScroll
+				{...rest}
+				loader={null}
+			>
+				{children}
+			</InfiniteScroll>
+		</Element>
+		{
+			rest.hasMore &&
+			<CircularProgress
+				size={40}
+				style={{ display: 'flex', alignItems: 'center', margin: '10px auto' }}
+			/>
+		}
+	</div>
+);
+
+InfiniteScrollWrapper.defaultProps = {
+	element: Paper,
+};
 
 export default InfiniteScrollWrapper;
