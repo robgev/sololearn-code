54f227592c6ab24a7dd484656708a2e8ac9ed210
Robert Gevorgyan <robert1999.g@gmail.com>
2018-07-17 15:57:11 +0400

    Add play files

---
+++ SoloLearn/app/Scripts/containers/Play/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router';
+import Paper from 'material-ui/Paper';
+import Layout from 'components/Layouts/GeneralLayout';
+import { RaisedButton } from 'material-ui';
+
+const NotFound = () => (
+	<Layout
+		noSidebar
+		style={{ width: 820, paddingRight: 0 }}
+	>
+		<Paper style={{
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'space-around',
+			alignItems: 'center',
+			height: '50vh',
+			padding: 15,
+		}}
+		>
+			<div className="flex-centered">
+				<h1>Under Construction</h1>
+				<h3>Play page is not ready yet.</h3>
+				<p>This page is under construction. It will be ready soon ;)</p>
+			</div>
+			<Link to="/">
+				<RaisedButton
+					labelColor="#fff"
+					label="Back to Home"
+					backgroundColor="#8bc34a"
+				/>
+			</Link>
+		</Paper>
+	</Layout>
+);
+
+export default NotFound;
+
+// rootStyle={{ { paddingTop: 0 }}
