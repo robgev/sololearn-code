9a6f26a544279ed8dcf39fab9ef3d0aed3b9c093
DavtyanErik <erikdavtyan@gmail.com>
2018-09-03 16:45:47 +0400

    Stop redirection on 'Follow' button click, remove red follower number when holding on 'Follow' button

--- SoloLearn/src/components/UserCard.js
+++ SoloLearn/src/components/UserCard.js
@@ -11,81 +11,91 @@ import 'styles/components/UserCard.scss';
 
 @connect(null, { followSuggestion })
 class UserCard extends Component {
-handleFollow = () => {
-	const { id, isFollowing } = this.props;
-	this.props.followSuggestion({ id, isFollowing })
-		.catch((e) => {
-			showError(e, `Something went wrong when trying to ${isFollowing ? 'unfollow' : 'follow'}`);
-			this.props.followSuggestion({ id, isFollowing });
-		});
-}
+	handleFollow = (event) => {
+		// Disables redirect to /profile/:id on button click
+		event.preventDefault();
 
-render() {
-	const {
-		id,
-		level,
-		name,
-		badge,
-		followers,
-		avatarUrl,
-		isFollowing,
-		className = '',
-		withFollowButton,
-	} = this.props;
-	const modBadge = determineBadge(badge);
-	return (
-		<div className={`discover-user-card-container ${className}`}>
-			<Link to={`/profile/${id}`} className="profile-container">
-				<div className="profile-avatar-wrapper">
-					{ avatarUrl
-						? <img src={avatarUrl} alt="avatar" className="profile-avatar" />
-						:	<div style={{ backgroundColor: AvatarColors[id % AvatarColors.length] }} className="profile-avatar">{name ? name.toUpperCase().charAt(0) : ''}</div>
-					}
-				</div>
-				<div className="profile-data">
-					<div className="profile-data-wrapper">
+		const { id, isFollowing } = this.props;
+		this.props.followSuggestion({ id, isFollowing })
+			.catch((e) => {
+				showError(e, `Something went wrong when trying to ${isFollowing ? 'unfollow' : 'follow'}`);
+				this.props.followSuggestion({ id, isFollowing });
+			});
+	}
 
-						<div className="profile-name">
-							{name}
-							<ModBadge
-								badge={modBadge}
-								className="small"
-							/>
-						</div>
-						{ (followers || followers === 0) &&
-						<div className="profile-followers">
-							{numberFormatter(followers)} Followers
-						</div>
-						}
-						{ (level || level === 0) &&
-						<div className="profile-followers">
-						Level {numberFormatter(level)}
-						</div>
+	render() {
+		const {
+			id,
+			level,
+			name,
+			badge,
+			followers,
+			avatarUrl,
+			isFollowing,
+			className = '',
+			withFollowButton,
+		} = this.props;
+		const modBadge = determineBadge(badge);
+		return (
+			<div className={`discover-user-card-container ${className}`}>
+				<Link to={`/profile/${id}`} style={{ textDecoration: 'none' }} className="profile-container">
+					<div className="profile-avatar-wrapper">
+						{avatarUrl
+							? <img src={avatarUrl} alt="avatar" className="profile-avatar" />
+							: (
+								<div
+									style={{ backgroundColor: AvatarColors[id % AvatarColors.length] }}
+									className="profile-avatar"
+								>
+									{name ? name.toUpperCase().charAt(0) : ''}
+								</div>
+							)
 						}
 					</div>
+					<div className="profile-data">
+						<div className="profile-data-wrapper">
 
-					{withFollowButton ?
-						<RaisedButton
-							secondary={isFollowing}
-							onClick={this.handleFollow}
-							style={{ height: 28, marginBottom: 5 }}
-							buttonStyle={{ height: 28, lineHeight: '28px' }}
-							className="user-card-follow-button"
-							label={isFollowing ? 'Following' : 'Follow'}
-						/> : (
-							<Link to={`/profile/${id}`} className="user-card-follow-button">
-								<RaisedButton
-									label="View Profile"
+							<div className="profile-name">
+								{name}
+								<ModBadge
+									badge={modBadge}
+									className="small"
 								/>
-							</Link>
-						)
-					}
-				</div>
-			</Link>
+							</div>
+							{(followers || followers === 0) &&
+								<div className="profile-followers">
+									{numberFormatter(followers)} Followers
+								</div>
+							}
+							{(level || level === 0) &&
+								<div className="profile-followers">
+									Level {numberFormatter(level)}
+								</div>
+							}
+						</div>
 
-		</div>
-	);
-}
+						{withFollowButton ?
+							<RaisedButton
+								secondary={isFollowing}
+								onClick={this.handleFollow}
+								style={{ height: 28, marginBottom: 5 }}
+								buttonStyle={{ height: 28, lineHeight: '28px' }}
+								className="user-card-follow-button"
+								label={isFollowing ? 'Following' : 'Follow'}
+							/> : (
+								<Link to={`/profile/${id}`} className="user-card-follow-button">
+									<RaisedButton
+										label="View Profile"
+									/>
+								</Link>
+							)
+						}
+					</div>
+				</Link>
+
+			</div>
+		);
+	}
 }
 
 export default UserCard;
