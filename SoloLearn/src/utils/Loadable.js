cd496c62f49c37bc4270a9de6ac843ee4b2d7b06
DavtyanErik <erikdavtyan@gmail.com>
2018-09-05 17:52:20 +0400

    Remove loadings before shimmer, show blank instead

--- SoloLearn/src/utils/Loadable.js
+++ SoloLearn/src/utils/Loadable.js
@@ -1,16 +1,5 @@
-import React from 'react';
 import Loadable from 'react-loadable';
-import CircularProgress from 'material-ui/CircularProgress';
 
-const Loading = () => (
-	<CircularProgress
-		size={40}
-		style={{
-			display: 'flex',
-			alignItems: 'center',
-			margin: 'auto',
-		}}
-	/>
-);
+const Loading = () => null;
 
 export default ({ loader }) => Loadable({ loader, loading: Loading });
