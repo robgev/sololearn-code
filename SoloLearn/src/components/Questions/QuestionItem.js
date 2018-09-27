62df0cdadb55f4cf8bdf60eb084bef2b34d9b84f
Robert Gevorgyan <robert1999.g@gmail.com>
2018-09-27 17:31:13 +0400

    [Discuss:Localize] Add several strings, change case of vote strings

--- SoloLearn/src/components/Questions/QuestionItem.js
+++ SoloLearn/src/components/Questions/QuestionItem.js
@@ -6,70 +6,56 @@ import Localize from 'components/Localize';
 // Utils
 import { removeDups, updateDate } from 'utils';
 
-import Likes from 'components/Likes';
-
 import 'styles/Discuss/QuestionItem.scss';
 import DiscussTag from './DiscussTag';
 
-class QuestionItem extends PureComponent {
-	state = {
-		isTooltipActive: false,
-	}
-	toggleTooltip = () => {
-		this.setState(state => ({ isTooltipActive: !state.isTooltipActive }));
-	}
-	render() {
-		const { isTooltipActive } = this.state;
-		const { question } = this.props;
-		return (
-			<Localize>
-				{({ t }) => (
-					<div className="question-item-container">
-						<Link to={`/discuss/${question.id}`} className="question-stats">
-							<div className="question-item-wrapper">
-								<div className="question-item-likes"> {question.votes} </div>
-								<p className="question-item-label">Votes</p>
-							</div>
-							<div className="question-item-wrapper">
-								<p className="question-item-answer-count">{question.answers > 99 ? '99+' : question.answers}</p>
-								<p className="question-item-label">
-									{question.answers === 1 ? t("discuss.answer-one-format") : t("discuss.answer-other-format")}
-								</p>
-							</div>
+const QuestionItem = ({ question }) => (
+	<Localize>
+		{({ t }) => (
+			<div className="question-item-container">
+				<Link to={`/discuss/${question.id}`} className="question-stats">
+					<div className="question-item-wrapper">
+						<div className="question-item-likes"> {question.votes} </div>
+						<p className="question-item-label">{t('discuss.votes-title')}</p>
+					</div>
+					<div className="question-item-wrapper">
+						<p className="question-item-answer-count">{question.answers > 99 ? '99+' : question.answers}</p>
+						<p className="question-item-label">
+							{question.answers === 1 ? t('discuss.answer-one-format') : t('discuss.answer-other-format')}
+						</p>
+					</div>
+				</Link>
+				<div className="question-item-details-wrapper">
+					<div>
+						<Link className="question-item-title-link hoverable" to={`/discuss/${question.id}`}>
+							{question.title}
 						</Link>
-						<div className="question-item-details-wrapper">
-							<div>
-								<Link className="question-item-title-link hoverable" to={`/discuss/${question.id}`}>
-									{question.title}
-								</Link>
-								<div>
-									{
-										removeDups(question.tags).map((tag, index) => (
-											<DiscussTag
-												key={`${question.id} ${tag}`}
-												tag={tag}
-												index={index}
-											/>
-										))
-									}
-								</div>
-							</div>
-							<div className="question-item-author-details">
-								<span className="question-item-date">
-									{updateDate(question.date)} by {' '}
-								</span>
-								<UserTooltip userData={question}>
-									<span>
-										{question.userName}
-									</span>
-								</UserTooltip>
-							</div>
+						<div>
+							{
+								removeDups(question.tags).map((tag, index) => (
+									<DiscussTag
+										key={`${question.id} ${tag}`}
+										tag={tag}
+										index={index}
+									/>
+								))
+							}
 						</div>
 					</div>
-				)}
-			</Localize>
-		);
-	}
-}
+					<div className="question-item-author-details">
+						<span className="question-item-date">
+							{updateDate(question.date)} by {' '}
+						</span>
+						<UserTooltip userData={question}>
+							<span>
+								{question.userName}
+							</span>
+						</UserTooltip>
+					</div>
+				</div>
+			</div>
+		)}
+	</Localize>
+);
 
 export default QuestionItem;
--- SoloLearn/src/components/Questions/QuestionList.js
+++ SoloLearn/src/components/Questions/QuestionList.js
@@ -10,42 +10,44 @@ import QuestionItem from './QuestionItem';
 export default observer(({
 	questions, loadMore, hasMore, header,
 }) =>
-	<Localize>
-		{({ t }) => 
-			(questions.length === 0 && !hasMore ? (
-				<Paper style={{
-					padding: 15,
-				}}
-				>
-					{header}
-					<div className="no-questions-wrapper">
-						{t("common.empty-list-message")}
-					</div>
-				</Paper>
-			) : (
-				<div>
-					{questions.length === 0 &&
+	(
+		<Localize>
+			{
+				({ t }) =>
+					(questions.length === 0 && !hasMore ? (
+						<Paper style={{
+							padding: 15,
+						}}
+						>
+							{header}
+							<div className="no-questions-wrapper">
+								{t('common.empty-list-message')}
+							</div>
+						</Paper>
+					) : (
+						<div>
+							{questions.length === 0 &&
 							(
 								<Paper style={{ height: '100vh', padding: 15, overflow: 'hidden' }}>
 									{header}
 									<DiscussShimmer />
 								</Paper>
 							)}
-					<InfiniteScroll
-						header={questions.length !== 0 ? header : null}
-						loadMore={loadMore}
-						hasMore={hasMore}
-						style={{
-							display: 'flex',
-							width: '100%',
-							padding: 15,
-							flexDirection: 'column',
-						}}
-					>
-						{questions.map(el => <QuestionItem key={el.id} question={el} />)}
-					</InfiniteScroll>
-				</div>
-			))
-		}
-	</Localize>
-);
+							<InfiniteScroll
+								header={questions.length !== 0 ? header : null}
+								loadMore={loadMore}
+								hasMore={hasMore}
+								style={{
+									display: 'flex',
+									width: '100%',
+									padding: 15,
+									flexDirection: 'column',
+								}}
+							>
+								{questions.map(el => <QuestionItem key={el.id} question={el} />)}
+							</InfiniteScroll>
+						</div>
+					))
+			}
+		</Localize>
+	));
