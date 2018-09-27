20694377c673b2f63903fa87ec5e198d331843b4
Robert Gevorgyan <robert1999.g@gmail.com>
2018-09-27 19:25:38 +0400

    [Global:Remove] Remove a lot of unnecessary stuff

--- SoloLearn/src/containers/__Challenges/Challenge/Game/gameUtil.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import 'styles/Challenges/Challenge/Game/GameUtil.scss'
-export const createAnswerUI = ({id, isCompleted}) => (
-    <div key={id}>
-        <img src={isCompleted ? '/assets/check_mark_right.png' : '/assets/red_cross_wrong.png'} className='answer-mark-img' />
-    </div>
-);
