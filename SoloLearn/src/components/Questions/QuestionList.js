3a66057857f873d808aba6ad2596718bec72817c
Erik Davtyan <erikdavtyan@gmail.com>
2018-10-17 18:39:05 +0400

    [Profile:Fix] Show questions on first render

--- SoloLearn/src/components/Questions/QuestionList.js
+++ SoloLearn/src/components/Questions/QuestionList.js
@@ -1,53 +1,50 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import { translate } from 'react-i18next';
 import InfiniteScroll from 'components/InfiniteScroll';
-import Localize from 'components/Localize';
 import Paper from 'material-ui/Paper';
 import DiscussShimmer from 'components/Shimmers/DiscussShimmer';
 import 'styles/Discuss/Questions.scss';
 import QuestionItem from './QuestionItem';
 
-export default observer(({
-	questions, loadMore, hasMore, header,
+const QuestionList = translate()(observer(({
+	questions, loadMore, hasMore, header, t,
 }) =>
 	(
-		<Localize>
-			{
-				({ t }) =>
-					(questions.length === 0 && !hasMore ? (
-						<Paper style={{
-							padding: 15,
-						}}
-						>
-							{header}
-							<div className="no-questions-wrapper">
-								{t('common.empty-list-message')}
-							</div>
-						</Paper>
-					) : (
-						<div>
-							{questions.length === 0 &&
-							(
-								<Paper style={{ height: '100vh', padding: 15, overflow: 'hidden' }}>
-									{header}
-									<DiscussShimmer />
-								</Paper>
-							)}
-							<InfiniteScroll
-								header={questions.length !== 0 ? header : null}
-								loadMore={loadMore}
-								hasMore={hasMore}
-								style={{
-									display: 'flex',
-									width: '100%',
-									padding: 15,
-									flexDirection: 'column',
-								}}
-							>
-								{questions.map(el => <QuestionItem key={el.id} question={el} />)}
-							</InfiniteScroll>
-						</div>
-					))
-			}
-		</Localize>
-	));
+		questions.length === 0 && !hasMore ? (
+			<Paper style={{
+				padding: 15,
+			}}
+			>
+				{header}
+				<div className="no-questions-wrapper">
+					{t('common.empty-list-message')}
+				</div>
+			</Paper>
+		) : (
+			<div>
+				{questions.length === 0 &&
+						(
+							<Paper style={{ height: '100vh', padding: 15, overflow: 'hidden' }}>
+								{header}
+								<DiscussShimmer />
+							</Paper>
+						)}
+				<InfiniteScroll
+					header={questions.length !== 0 ? header : null}
+					loadMore={loadMore}
+					hasMore={hasMore}
+					style={{
+						display: 'flex',
+						width: '100%',
+						padding: 15,
+						flexDirection: 'column',
+					}}
+				>
+					{questions.map(el => <QuestionItem key={el.id} question={el} />)}
+				</InfiniteScroll>
+			</div>
+		)
+	)));
+
+export default QuestionList;
