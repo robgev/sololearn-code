05fdd82e04a2fed1d3720520c5750603f4b210c1
Erik Davtyan <erikdavtyan@gmail.com>
2018-10-25 17:03:51 +0400

    [Design] Change all external link colors to #607D8B

--- SoloLearn/src/components/ExternalLink.js
+++ SoloLearn/src/components/ExternalLink.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Linkify from 'react-linkify';
 
+const style = { color: '#607D8B' };
+
 /* eslint-disable */
 
-export const ExternalLink = props => <a target="_blank" rel="noopener noreferrer" {...props} />;
+export const ExternalLink = props => <a style={style} target="_blank" rel="noopener noreferrer" {...props} />;
 
 export const ExternalLinkify = props => 
-  <Linkify properties={{ target: '_blank', rel: 'noopener noreferrer' }} {...props} />;
+  <Linkify properties={{ target: '_blank', rel: 'noopener noreferrer', style }} {...props} />;
 
 export default ExternalLink;
