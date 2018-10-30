441b7145b5cfac0941720cf32018bfa2847ca979
sevakekizyan <sevakekizyan@gmail.com>
2018-10-30 15:04:56 +0400

    [Popover atom]

---
+++ SoloLearn/src/components/atoms/Popover/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Tooltip } from 'react-tippy';
+import './styles.scss';
+
+const Popover = ({ className, ...props }) =>
+	<Tooltip 
+		arrow
+		interactive
+		useContext
+		tabIndex="0"
+		theme="light"
+		position="top-start"
+		unmountHTMLWhenHide
+		className={'atom_popover ' + className} 
+		{...props} 
+	/>;
+
+Popover.defaultProps = {
+	className: '',
+};
+export default Popover;
--- SoloLearn/src/components/atoms/index.js
+++ SoloLearn/src/components/atoms/index.js
@@ -3,3 +3,4 @@ export { default as Link } from './Link';
 export { default as MenuItem } from './MenuItem';
 export { default as Checkbox } from './Checkbox';
 export { default as RadioButton } from './RadioButton';
+export { default as Popover } from './Popover';
