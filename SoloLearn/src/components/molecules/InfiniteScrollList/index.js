426b7781fe9d087d68ffaed1f0b4af389cbb4d0d
Erik Davtyan <erikdavtyan@gmail.com>
2018-10-31 15:44:06 +0400

    [InfiniteScrollList molecule] Add prop types

--- SoloLearn/src/components/molecules/InfiniteScrollList/index.js
+++ SoloLearn/src/components/molecules/InfiniteScrollList/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import InfiniteScroll from 'react-infinite-scroller';
 import Loading from '@material-ui/core/CircularProgress';
 
@@ -13,9 +14,10 @@ const InfiniteScrollList = ({ children, isLoading, ...props }) => (
 	</Fragment>
 );
 
-InfiniteScrollList.defaultProps = {
-	hasMore: false,
-	isLoading: false,
+InfiniteScrollList.propTypes = {
+	hasMore: PropTypes.bool.isRequired,
+	isLoading: PropTypes.bool.isRequired,
+	loadMore: PropTypes.func.isRequired,
 };
 
 export default InfiniteScrollList;
