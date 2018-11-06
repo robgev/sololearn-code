fc22c2df04dbd64c453da72ba9ef8664287beeb1
Robert Gevorgyan <robert1999.g@gmail.com>
2018-11-06 15:28:20 +0400

    [General:Fix] Change default styles of IconButton to avoid outer hover action

--- SoloLearn/src/components/atoms/IconButton/index.js
+++ SoloLearn/src/components/atoms/IconButton/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
+import MUIIconButton from '@material-ui/core/IconButton';
+import './styles.scss';
 
-const IconButtonWrapper = props => (
-	<IconButton {...props} />
+const IconButton = props => (
+	<MUIIconButton classes={{ root: 'atom_icon-button-root' }} {...props} />
 );
 
-export default IconButtonWrapper;
+export default IconButton;
