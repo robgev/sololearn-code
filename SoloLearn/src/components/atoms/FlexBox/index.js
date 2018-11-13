d47677f1ede63bb177b6942a273e92886cf5246e
Erik Davtyan <erikdavtyan@gmail.com>
2018-11-13 16:16:30 +0400

    [Atom:FlexBox]

---
+++ SoloLearn/src/components/atoms/FlexBox/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Container from '../Container';
+
+const FlexBox = ({
+	justify, align, column, className, ...props
+}) => {
+	const columnClassName = column ? 'column' : '';
+	const justifyClassName = justify ? 'justify-center' : '';
+	const alignClassName = align ? 'align-center' : '';
+	const fullClassName = `atom_flexbox ${columnClassName} ${justifyClassName} ${alignClassName} ${className}`;
+	return (
+		<Container
+			className={fullClassName}
+			{...props}
+		/>
+	);
+};
+
+FlexBox.defaultProps = {
+	className: '',
+	column: false,
+	justify: false,
+	align: false,
+};
+
+export default FlexBox;
