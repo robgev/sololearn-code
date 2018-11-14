da36a2a2c038382ccaa0b7fdf03d6a75336e9ad7
Erik Davtyan <erikdavtyan@gmail.com>
2018-11-14 17:37:27 +0400

    [Molecule:FAB] Change FAB to be of fixed height

--- SoloLearn/src/components/molecules/FloatingActionButton/index.js
+++ SoloLearn/src/components/molecules/FloatingActionButton/index.js
@@ -4,17 +4,18 @@ import './styles.scss';
 
 // Note, when using this, please set position: relative to the container
 // you want to have the button in.
-// Also, set z-index: 1 to all elements that should be under button.
 
 const FloatingActionButton = ({
 	alignment,
 	className,
 	children,
+	onClick,
 	...props
 }) =>
 	(
 		<Container className={`molecule_fab ${alignment} ${className}`} {...props}>
 			<Button
+				onClick={onClick}
 				variant="fab"
 				color="secondary"
 				classes={{ root: 'molecule_fab-button-root' }}
