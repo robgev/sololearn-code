3db31b5a9468a816eac5088741c6347328c77412
sevakekizyan <sevakekizyan@gmail.com>
2018-11-16 11:09:48 +0400

    [RadioButton atom] RadioButton atom label

--- SoloLearn/src/components/atoms/RadioButton/index.js
+++ SoloLearn/src/components/atoms/RadioButton/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Radio from '@material-ui/core/Radio';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { TextBlock } from 'components/atoms';
 
-const RadioButton = props => (
-	<Radio {...props} />
+const RadioButton = ({label, ...props}) => (
+	<FormControlLabel
+		value="left"
+		control={<Radio {...props} />}
+		label={<TextBlock>{label}</TextBlock>}
+		labelPlacement="left"
+	/>
+	
 );
 
 export default RadioButton;
