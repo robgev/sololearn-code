de120751fd081ad2c55641ad5a369a1a7ada5843
Erik Davtyan <erikdavtyan@gmail.com>
2018-11-19 19:25:46 +0400

    [Quiz Factory] Partially integrate new components

--- SoloLearn/src/containers/QuizFactory/Layout.js
+++ SoloLearn/src/containers/QuizFactory/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { LayoutWithSidebar } from 'components/molecules';
 import Sidebar from './components/Sidebar';
+import './style.scss';
 
 const Layout = props => (
 	<LayoutWithSidebar
 		sidebar={
 			<Sidebar />
 		}
+		className="quiz_factory"
 		{...props}
 	/>
 );
