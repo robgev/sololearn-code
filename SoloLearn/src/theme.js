4bf52ed56a544d7ab0cc4c01ab6ea548cc459f6b
Robert Gevorgyan <robert1999.g@gmail.com>
2018-11-27 21:41:10 +0400

    [General:Theme] Add dark and light colors to primary

--- SoloLearn/src/theme.js
+++ SoloLearn/src/theme.js
@@ -4,6 +4,9 @@ export default createMuiTheme({
 	palette: {
 		primary: {
 			main: '#607D8B',
+			dark: '#587583',
+			light: '#7A97A5',
+			contrastText: 'white',
 		},
 		secondary: {
 			main: '#8BC34A',
