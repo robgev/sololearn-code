3f0ad99c2db0bfe03d2d6aa5531e9a6bbae28e7a
Robert Gevorgyan <robert1999.g@gmail.com>
2018-11-27 21:46:49 +0400

    [General:Fix] Minor changes in atoms

--- SoloLearn/src/components/atoms/Input/index.js
+++ SoloLearn/src/components/atoms/Input/index.js
@@ -4,7 +4,11 @@ import TextField from '@material-ui/core/TextField';
 import './styles.scss';
 
 const Input = props => (
-	<TextField InputProps={{classes:{underline:'atom_input_underline'}}} InputLabelProps={{FormLabelClasses:{root:'atom_input_label',focused:'atom_input_label_focused'}}} {...props} />
+	<TextField
+		{...props}
+		InputProps={{ classes: { underline: 'atom_input_underline' } }}
+		InputLabelProps={{ FormLabelClasses: { root: 'atom_input_label', focused: 'atom_input_label_focused' } }}
+	/>
 );
 
 export default Input;
--- SoloLearn/src/components/atoms/Tab/index.js
+++ SoloLearn/src/components/atoms/Tab/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import MUITab from '@material-ui/core/Tab';
-
 import './styles.scss';
 
-const Tab = ({ ...props }) => (
+const Tab = props => (
 	<MUITab classes={{ root: 'atom_tab' }} {...props} />
 );
 
