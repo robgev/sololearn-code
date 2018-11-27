c20176165be6c54b9633b5f230db1d4b6d5e8191
Erik Davtyan <erikdavtyan@gmail.com>
2018-11-27 15:48:24 +0400

    [Learn] Fix modules page loading and heading styling

--- SoloLearn/src/containers/Learn/Modules/index.js
+++ SoloLearn/src/containers/Learn/Modules/index.js
@@ -1,20 +1,20 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 
 import { MoreVert } from 'components/icons';
 import {
-	Title,
+	Heading,
 	Loading,
 	MenuItem,
-	Container,
 	PaperContainer,
 	Popup,
 	PopupTitle,
 	PopupContent,
 	PopupActions,
 	PopupContentText,
+	FlexBox,
 } from 'components/atoms';
 import {
 	IconMenu,
@@ -85,116 +85,119 @@ class Modules extends Component {
 		this.setState({ loading: false });
 	}
 
-handleClick = (e, moduleId, moduleState) => {
-	if (moduleState.visualState === ProgressState.Disabled) {
-		e.preventDefault();
-		return;
-	}
-	this.props.selectModule(moduleId);
-}
-
-toggleCourse = (courseId, enable) => {
-	this.props.toggleCourseInternal(courseId, enable);
-}
-
-resetProgress = () => {
-	const courseId = this.editableCourseId;
-	this.handleResetPopupClose();
-
-	const { skills } = this.props.userProfile;
-	const index = skills.findIndex(item => item.id === courseId);
-	skills[index].progress = 0;
-
-	this.props.toggleCourse(skills);
-	Progress.reset();
-	Service.request('ResetProgress', { courseId });
-}
-
-handleResetPopupOpen = (courseId) => {
-	this.editableCourseId = courseId;
-	this.setState({ resetPopupOpened: true });
-}
-
-handleResetPopupClose = () => {
-	this.setState({ resetPopupOpened: false });
-}
-
-render() {
-	const {
-		t,
-		course,
-		params: { itemType, courseName },
-		isLoaded: isModuleLoaded,
-	} = this.props;
-	const { loading, resetPopupOpened } = this.state;
-	if (!isModuleLoaded && this.props.userProfile.skills.length > 0) {
-		return <Loading />;
+	handleClick = (e, moduleId, moduleState) => {
+		if (moduleState.visualState === ProgressState.Disabled) {
+			e.preventDefault();
+			return;
+		}
+		this.props.selectModule(moduleId);
 	}
 
-	const { modules, id } = course;
-	const userCourses = this.props.userProfile.skills;
+	toggleCourse = (courseId, enable) => {
+		this.props.toggleCourseInternal(courseId, enable);
+	}
 
-	return (
-		<LayoutWithSidebar sidebar={<UserProgressToolbar />}>
-			{ loading ? <Loading /> :
-				userCourses.length > 0
+	resetProgress = () => {
+		const courseId = this.editableCourseId;
+		this.handleResetPopupClose();
+
+		const { skills } = this.props.userProfile;
+		const index = skills.findIndex(item => item.id === courseId);
+		skills[index].progress = 0;
+
+		this.props.toggleCourse(skills);
+		Progress.reset();
+		Service.request('ResetProgress', { courseId });
+	}
+
+	handleResetPopupOpen = (courseId) => {
+		this.editableCourseId = courseId;
+		this.setState({ resetPopupOpened: true });
+	}
+
+	handleResetPopupClose = () => {
+		this.setState({ resetPopupOpened: false });
+	}
+
+	render() {
+		const {
+			t,
+			course,
+			params: { itemType, courseName },
+			isLoaded: isModuleLoaded,
+		} = this.props;
+		const { loading, resetPopupOpened } = this.state;
+		const { modules, id } = course;
+		const userCourses = this.props.userProfile.skills;
+
+		return (
+			<LayoutWithSidebar sidebar={<UserProgressToolbar />}>
+				{loading || (!isModuleLoaded && this.props.userProfile.skills.length > 0)
 					? (
 						<PaperContainer>
-							<Container className="course-selector">
-								<Title>{course.name}</Title>
-								<IconMenu
-									icon={MoreVert}
-									anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-									transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-								>
-									<MenuItem onClick={() => this.handleResetPopupOpen(course.id)}>
-										{t('course_picker.action.reset-progress')}
-									</MenuItem>
-								</IconMenu>
-							</Container>
-							<ModuleChips
-								courseId={id}
-								modules={modules}
-								itemType={itemType}
-								onClick={this.handleClick}
-								courseName={courseName}
-							/>
-							<Certificate
-								courseId={id}
-								modules={modules}
-							/>
-							<ModuleChip
-								state="normal"
-								className="center"
-								completionPercent={100}
-								to={`/learn/more-on/${id}`}
-								name={t('learn.more-on-topic')}
-								iconSource="https://api.sololearn.com/uploads/Courses/assets/more.png"
-							/>
+							<FlexBox className="empty-card-placeholder" align justify>
+								<Loading />
+							</FlexBox>
 						</PaperContainer>
 					)
-					:	<AddCourse />
-			}
-			<Popup
-				open={resetPopupOpened}
-				onClose={this.handleResetPopupClose}
-			>
-				<PopupTitle>{t('learn.reset-course-popup-title')}</PopupTitle>
-				<PopupContent>
-					<PopupContentText>{t('learn.reset-course-popup-message')}</PopupContentText>
-				</PopupContent>
-				<PopupActions>
-					<FlatButton onClick={this.handleResetPopupClose}>
-						{t('common.cancel-title')}
-					</FlatButton>
-					<FlatButton onClick={this.resetProgress}>
-						{t('learn.reset-course-popup-title')}
-					</FlatButton>
-				</PopupActions>
-			</Popup>
-		</LayoutWithSidebar>
-	);
-}
+					: userCourses.length > 0
+						? (
+							<PaperContainer>
+								<FlexBox justifyBetween>
+									<Heading>{course.name}</Heading>
+									<IconMenu
+										icon={MoreVert}
+										anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+										transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+									>
+										<MenuItem onClick={() => this.handleResetPopupOpen(course.id)}>
+											{t('course_picker.action.reset-progress')}
+										</MenuItem>
+									</IconMenu>
+								</FlexBox>
+								<ModuleChips
+									courseId={id}
+									modules={modules}
+									itemType={itemType}
+									onClick={this.handleClick}
+									courseName={courseName}
+								/>
+								<Certificate
+									courseId={id}
+									modules={modules}
+								/>
+								<ModuleChip
+									state="normal"
+									className="center"
+									completionPercent={100}
+									to={`/learn/more-on/${id}`}
+									name={t('learn.more-on-topic')}
+									iconSource="https://api.sololearn.com/uploads/Courses/assets/more.png"
+								/>
+							</PaperContainer>
+						)
+						: <AddCourse />
+				}
+				<Popup
+					open={resetPopupOpened}
+					onClose={this.handleResetPopupClose}
+				>
+					<PopupTitle>{t('learn.reset-course-popup-title')}</PopupTitle>
+					<PopupContent>
+						<PopupContentText>{t('learn.reset-course-popup-message')}</PopupContentText>
+					</PopupContent>
+					<PopupActions>
+						<FlatButton onClick={this.handleResetPopupClose}>
+							{t('common.cancel-title')}
+						</FlatButton>
+						<FlatButton onClick={this.resetProgress}>
+							{t('learn.reset-course-popup-title')}
+						</FlatButton>
+					</PopupActions>
+				</Popup>
+			</LayoutWithSidebar>
+		);
+	}
 }
 
 export default Modules;
