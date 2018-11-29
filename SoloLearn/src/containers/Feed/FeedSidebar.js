53a087121ca233294663371c25e98589e76400a0
Erik Davtyan <erikdavtyan@gmail.com>
2018-11-29 18:11:12 +0400

    [Feed:Sidebar] Fix styles

--- SoloLearn/src/containers/Feed/FeedSidebar.js
+++ SoloLearn/src/containers/Feed/FeedSidebar.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
-import ProfileAvatar from './ProfileAvatar';
 import SidebarShimmer from 'components/Shimmers/SidebarShimmer';
 import { numberFormatter } from 'utils';
-import { discoverIdsSelector, discoverEntitiesSelector } from 'reducers/discover.reducer.js';
+import { discoverIdsSelector, discoverEntitiesSelector } from 'reducers/discover.reducer';
 import {
-	Container,
-	Title,
-	Link,
-	SecondaryTextBlock,
+	Container, FlexBox, Heading, HorizontalDivider,
+	List, ListItem, SecondaryTextBlock,
 } from 'components/atoms';
-import { FlatButton, UsernameLink, } from 'components/molecules';
-
-import 'styles/Feed/FeedSidebar.scss';
+import { UsernameLink, ViewMoreLink } from 'components/molecules';
+import ProfileAvatar from './ProfileAvatar';
+import './sidebar.scss';
 
 const mapStateToProps = state => ({
 	discoverIds: discoverIdsSelector(state),
@@ -20,41 +17,49 @@ const mapStateToProps = state => ({
 });
 
 const FeedSuggestions = ({ t, discoverIds, discoverEntities }) => (
-	<Container className="feed-sidebar-suggestions">
-		<Container className="sidebar-title">
-			<Title className="title">{t('discover_peers.title')}</Title>
-		</Container>
+	<Container className="feed-sidebar">
+		<Heading>{t('discover_peers.title')}</Heading>
 		{discoverEntities === null
 			? <SidebarShimmer round noTitle />
-			: discoverIds.slice(0, 7).map((id) => {
-				const {
-					name, avatarUrl, followers, level,
-				} = discoverEntities[id];
-				return (
-					<Container className="suggestion-container">
-						<ProfileAvatar
-							user={discoverEntities[id]}
-						/>
-						<Container className="user-info">
-							<UsernameLink to={`/profile/${id}`} >{name}</UsernameLink>
-
-							<SecondaryTextBlock className="user-meta-info">
-								{
-									followers === 1
-										? `1 ${t('user-followers-one')}`
-										: `${numberFormatter(followers)} ${t('common.user-followers')}`
-								} | {t('common.user-level')} {numberFormatter(level)}
-							</SecondaryTextBlock>
-						</Container>
-					</Container>
-				);
-			})}
-		{ discoverEntities && discoverIds.length > 0 &&
-			<Link className="load-more" to="/discover">
-				<FlatButton>
-					{t('common.loadMore')}
-				</FlatButton>
-			</Link>
+			: (
+				<List>
+					{
+						discoverIds.slice(0, 7).map((id) => {
+							const {
+								name, followers, level,
+							} = discoverEntities[id];
+							return (
+								<Fragment>
+									<ListItem>
+										<FlexBox>
+											<ProfileAvatar
+												className="profile"
+												user={discoverEntities[id]}
+											/>
+											<FlexBox column>
+												<UsernameLink to={`/profile/${id}`} >{name}</UsernameLink>
+												<SecondaryTextBlock>
+													{
+														followers === 1
+															? `1 ${t('user-followers-one')}`
+															: `${numberFormatter(followers)} ${t('common.user-followers')}`
+													} | {t('common.user-level')} {numberFormatter(level)}
+												</SecondaryTextBlock>
+											</FlexBox>
+										</FlexBox>
+									</ListItem>
+									<HorizontalDivider />
+								</Fragment>
+							);
+						})
+					}
+				</List>
+			)
+		}
+		{discoverEntities && discoverIds.length > 0 &&
+			<ViewMoreLink to="/discover">
+				{t('common.loadMore')}
+			</ViewMoreLink>
 		}
 	</Container>
 );
