cc0aa0929627b787a4b248cbeaf9aaae91049b83
sevakekizyan <sevakekizyan@gmail.com>
2018-12-04 16:19:58 +0400

    [Parser]

--- SoloLearn/src/containers/Learn/components/Parser/index.js
+++ SoloLearn/src/containers/Learn/components/Parser/index.js
@@ -38,7 +38,7 @@ class Parser extends Component {
 
 	// static utility funcitons and regexes
 
-	static tagRegex = /\[(b|i|u|h1|h2|h3|note|code|a)( .*)?\]([\s\S\n]*?)\[\/\1\]/;
+	static tagRegex = /\[(b|i|u|h1|h2|h3|note|code|a)(\s+[^\]]*)?\]([\s\S\n]*?)\[\/\1\]/;
 
 	static imgRegex = /\[img id="(\d+)" width="(\d+)%"\]/;
 
@@ -181,6 +181,7 @@ class Parser extends Component {
 	}
 	parse = () => {
 		const { courseLanguage } = this.props;
+		console.log(this.text);
 		const toBeParsed = this.text.replace('\r\n\r\n', '\r\n');
 		return this._parse({ text: toBeParsed, courseLanguage });
 	}
