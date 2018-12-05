58ce7c4884a2dbb62eec4da3e6e50a317bb2a834
Erik Davtyan <erikdavtyan@gmail.com>
2018-12-05 19:12:20 +0400

    [Discuss] Bring shimmers back

--- SoloLearn/src/containers/Discuss/QuestionsList/QuestionList.js
+++ SoloLearn/src/containers/Discuss/QuestionsList/QuestionList.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import { List } from 'components/atoms';
 import { EmptyCard } from 'components/molecules';
+import DiscussShimmer from 'components/Shimmers/DiscussShimmer';
 import Question from './Question';
 
 const QuestionList = ({ questions, hasMore }) => (
 	questions.length > 0 || hasMore
-		? (
-			<List>
-				{
-					questions.map(question => (
-						<Question key={question.id} question={question} />
-					))
-				}
-			</List>
+		? (questions.length === 0
+			? <DiscussShimmer />
+			: (
+				<List>
+					{
+						questions.map(question => (
+							<Question key={question.id} question={question} />
+						))
+					}
+				</List>
+			)
 		)
 		: <EmptyCard />
 );
