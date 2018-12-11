02c2a5bde63e6cb7a3eb8410ed2b5b81a48a35f5
Robert Gevorgyan <robert1999.g@gmail.com>
2018-12-11 19:12:28 +0400

    [Leaderboards:Fix] Add mod badge

--- SoloLearn/src/containers/Leaderboards/UserCard.js
+++ SoloLearn/src/containers/Leaderboards/UserCard.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { ProfileAvatar, UsernameLink, } from 'components/molecules';
-import { Container, SecondaryTextBlock, } from 'components/atoms';
+import { ModBadge, ProfileAvatar, UsernameLink } from 'components/molecules';
+import { Container, SecondaryTextBlock } from 'components/atoms';
 import './UserCard.scss';
 
-const UserData = ({ name, xp, id }) => (
+const UserData = ({
+	name, xp, id, badge,
+}) => (
 	<Container className="user-meta-data">
-		<UsernameLink to={`/profile/${id}`}>{ name }</UsernameLink>
+		<UsernameLink to={`/profile/${id}`}>
+			{ name }
+		</UsernameLink>
+		<ModBadge
+			className="badge"
+			badge={badge}
+		/>
 		<SecondaryTextBlock className="info">+{xp} XP</SecondaryTextBlock>
 	</Container>
 );
 
 const UserCard = ({
-	user
+	user,
 }) => (
 	<Container className="user-card-container">
 		<Container className="user-info">
 			<ProfileAvatar user={user} />
-			<UserData {...user}/>
+			<UserData {...user} />
 		</Container>
 		<Container>{ user.rank }</Container>
 	</Container>
