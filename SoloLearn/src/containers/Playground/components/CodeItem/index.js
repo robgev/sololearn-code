5683d36fba96c887f20aba89de194f454aea30dc
Robert Gevorgyan <robert1999.g@gmail.com>
2018-12-13 16:19:55 +0400

    [Leaderboard:Fix] Adjust leaderboard link, fix some minor bugs in PG

--- SoloLearn/src/containers/Playground/components/CodeItem/index.js
+++ SoloLearn/src/containers/Playground/components/CodeItem/index.js
@@ -1,33 +1,42 @@
 import React, { Fragment } from 'react';
 import { Lock } from 'components/icons';
-import { Link, Title, Container, ListItem, HorizontalDivider } from 'components/atoms';
+import { FlexBox, Link, Title, Container, ListItem, HorizontalDivider } from 'components/atoms';
 import { ViewStats, ProfileAvatar, LanguageLabel } from 'components/molecules';
 
 import './styles.scss';
 
-const CodeItem = ({ code }) => (
-	<Fragment>
-		<ListItem className="code-item-wrapper">
-			<ProfileAvatar className="user" user={code} />
-			<Container className="details-wrapper">
-				<Link to={`/playground/${code.publicID}`}>
-					<Title>
-						{code.name}
-					</Title>
-				</Link>
-				<Container className="stats">
-					<LanguageLabel language={code.language} />
-					<ViewStats votes={code.votes} comments={code.comments} />
-					<Container>
-						{!code.isPublic &&
+const CodeItem = ({ code }) => {
+	const user = {
+		id: code.userID,
+		name: code.userName,
+		badge: code.badge,
+		level: code.level,
+		avatarUrl: code.avatarUrl,
+	};
+	return (
+		<Fragment>
+			<ListItem className="code-item-wrapper">
+				<ProfileAvatar className="user" user={user} />
+				<FlexBox fullWidth column className="details-wrapper">
+					<Link to={`/playground/${code.publicID}`}>
+						<Title>
+							{code.name}
+						</Title>
+					</Link>
+					<Container className="stats">
+						<LanguageLabel language={code.language} />
+						<ViewStats votes={code.votes} comments={code.comments} />
+						<Container>
+							{!code.isPublic &&
 							<Lock />
-						}
+							}
+						</Container>
 					</Container>
-				</Container>
-			</Container>
-		</ListItem>
-		<HorizontalDivider />
-	</Fragment>
-);
+				</FlexBox>
+			</ListItem>
+			<HorizontalDivider />
+		</Fragment>
+	);
+};
 
 export default CodeItem;
