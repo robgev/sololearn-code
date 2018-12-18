4d2b5950b358fb03dc25b765d36801064e3ffc51
Robert Gevorgyan <robert1999.g@gmail.com>
2018-12-18 12:58:31 +0400

    [Leaderboard:Fix] Show gained XP instead of all XP

--- SoloLearn/src/containers/Leaderboards/LeaderboardCard.js
+++ SoloLearn/src/containers/Leaderboards/LeaderboardCard.js
@@ -1,47 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router';
 import VisibilitySensor from 'react-visibility-sensor';
 import texts from 'texts';
-import UserCard from './UserCard';
 import { Container, HorizontalDivider } from 'components/atoms';
-
+import UserCard from './UserCard';
 
 const LeaderboardCard = ({
 	userId,
 	userRank,
 	leaderboards,
 	onScrollVisibility,
-}) => {
-	return (
+}) => (
 	<Container className="leaderboard-card-container">
-		{	
-			leaderboards.map((user, index) => {
-				user.id = user.userID;
-				return (
+		{
+			leaderboards.map((user, index) => (
 				<Container className="leaderboard-card-wrapper" key={user.name}>
 					{ (index === 10 && userRank > 10) && // after top 10
-					<Container className="leaderboard-next-banner">
-						{ texts.next }
-					</Container>
+						<Container className="leaderboard-next-banner">
+							{ texts.next }
+						</Container>
 					}
-					<Container className={`leaderboard-card ${user.userID === userId ? ' highlighted':''}`} id={`user-card-${user.userID}`}>
-					{	user.userID === userId ?
-						<VisibilitySensor
-							scrollCheck
-							scrollThrottle={100}
-							onChange={onScrollVisibility}
-						>
+					<Container className={`leaderboard-card ${user.userID === userId ? ' highlighted' : ''}`} id={`user-card-${user.userID}`}>
+						{	user.userID === userId ?
+							<VisibilitySensor
+								scrollCheck
+								scrollThrottle={100}
+								onChange={onScrollVisibility}
+							>
+								<UserCard user={user} />
+							</VisibilitySensor> :
 							<UserCard user={user} />
-						</VisibilitySensor> :
-						<UserCard user={user} />
-					}
+						}
 					</Container>
-					<HorizontalDivider/>
+					<HorizontalDivider />
 				</Container>
-			)}
-				)
+			))
 		}
 	</Container>
-);}
+);
 
 export default LeaderboardCard;
--- SoloLearn/src/containers/Leaderboards/UserCard.js
+++ SoloLearn/src/containers/Leaderboards/UserCard.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { ModBadge, ProfileAvatar, UsernameLink } from 'components/molecules';
-import { Container, SecondaryTextBlock } from 'components/atoms';
+import { FlexBox, Container, SecondaryTextBlock } from 'components/atoms';
 import './UserCard.scss';
 
 const UserData = ({
-	name, xp, id, badge,
+	name, rangeXp, userID, badge,
 }) => (
 	<Container className="user-meta-data">
-		<UsernameLink to={`/profile/${id}`}>
+		<UsernameLink to={`/profile/${userID}`}>
 			{ name }
 		</UsernameLink>
 		<ModBadge
 			className="badge"
 			badge={badge}
 		/>
-		<SecondaryTextBlock className="info">+{xp} XP</SecondaryTextBlock>
+		<SecondaryTextBlock className="info">+{rangeXp} XP</SecondaryTextBlock>
 	</Container>
 );
 
 const UserCard = ({
 	user,
 }) => (
-	<Container className="user-card-container">
-		<Container className="user-info">
-			<ProfileAvatar user={user} />
+	<FlexBox justifyBetween align>
+		<FlexBox>
+			<ProfileAvatar user={{ ...user, id: user.userID }} />
 			<UserData {...user} />
-		</Container>
+		</FlexBox>
 		<Container>{ user.rank }</Container>
-	</Container>
+	</FlexBox>
 );
 
 export default UserCard;
