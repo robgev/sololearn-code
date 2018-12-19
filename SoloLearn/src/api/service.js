8e0515431ec76d7fe927d6261c17d441c92ae2c5
sevakekizyan <sevakekizyan@gmail.com>
2018-12-19 16:13:59 +0400

    [General] Remove last / from downloadHost

--- SoloLearn/src/api/service.js
+++ SoloLearn/src/api/service.js
@@ -2,7 +2,7 @@ import Storage from './storage';
 
 export const AppDefaults = {
 	baseUrl: 'http://localhost:2525',
-	downloadHost: 'https://api.sololearn.com/uploads/',
+	downloadHost: 'https://api.sololearn.com/uploads',
 };
 
 class Service {
