a3062a8f2e4199b42f329ce48e6c46262cb88e24
Sokrat Poghosyan <sokrat.poghosyan@gmail.com>
2019-05-13 16:35:02 +0400

    Removed unnecessary button

--- SoloLearn/src/containers/UserPostEditor/EditorActions/index.js
+++ SoloLearn/src/containers/UserPostEditor/EditorActions/index.js
@@ -10,13 +10,6 @@ const EditorActions = ({ isPostButtonDisabled, createNewPostHandler }) => (
 	<FlexBox justifyEnd className="user-post-actions-container">
 		<PromiseButton
 			raised
-			fire={() => getUserPost(97742)}
-			style={{ marginRight: '10px' }}
-		>
-			Get Post (test)
-		</PromiseButton>
-		<PromiseButton
-			raised
 			color="primary"
 			disabled={isPostButtonDisabled}
 			fire={createNewPostHandler}
