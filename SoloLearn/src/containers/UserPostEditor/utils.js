4aea4b2d23be545a1cb54b7c3f6fc0d6972dd0f2
Sokrat Poghosyan <sokrat.poghosyan@gmail.com>
2019-05-15 15:02:39 +0400

    UP editor font-sizes, mobile sync

--- SoloLearn/src/containers/UserPostEditor/utils.js
+++ SoloLearn/src/containers/UserPostEditor/utils.js
@@ -29,10 +29,14 @@ export const getBackgroundStyle = (background, { isPreview }) => {
 };
 
 export const getFontSize = (textLength, newLineCount) => {
-	if (textLength < 200) {
-		return 16;
+	if (textLength < 50) {
+		if (newLineCount < 5) { return 36; }
+		return 26;
+	} else if (textLength < 200) {
+		if (newLineCount < 5) { return 22; }
+		return 20;
 	} else if (textLength >= 200 && textLength < 350) {
-		if (newLineCount < 5) { return 13; }
+		if (newLineCount < 5) { return 18; }
 		return 16;
-	} else if (textLength >= 350) { return 13; }
+	} else if (textLength >= 350) { return 14; }
 };
