3f169988da70844790f05314ccc4c0b85c15dc30
Erik Davtyan <erikdavtyan@gmail.com>
2019-05-22 16:03:20 +0400

    Send user post impressions when the post is seen

--- SoloLearn/src/containers/Feed/FeedTemplates/UserPost.js
+++ SoloLearn/src/containers/Feed/FeedTemplates/UserPost.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { Container, Image } from 'components/atoms';
+import React, { useRef } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
+import { Image } from 'components/atoms';
 import { ContainerLink } from 'components/molecules';
+import Service from 'api/service';
 
 import UserPostEditor from 'containers/UserPostEditor/DraftEditor';
 import { FeedBottomBarFullStatistics } from 'components/organisms';
+
+const sendImpressionByPostId = id =>
+	Service.request(`Profile/AddPostImpression?id=${id}`);
 
 const UserPost = ({
 	background,
@@ -18,28 +23,55 @@ const UserPost = ({
 	userPostId,
 	comments,
 	views,
-}) => (
-	<ContainerLink to={`post/${userPostId}`}>
-		{message ?
-			<UserPostEditor
-				measure={measure || (() => {})}
-				background={background || { type: 'none', id: -1 }}
-				editorInitialText={message}
-				isEditorReadOnly
-			/>
-			: null
+}) => {
+	const impressionTimeoutIdRef = useRef();
+
+	const cancelImpressionTimer = () => {
+		if (impressionTimeoutIdRef.current) {
+			clearTimeout(impressionTimeoutIdRef.current);
 		}
-		{imageUrl ? <Image src={imageUrl} onLoad={measure || (() => {})} style={{ maxWidth: '400px' }} alt="" /> : null}
-		<FeedBottomBarFullStatistics
-			type={type}
-			date={date}
-			id={id}
-			userVote={vote}
-			totalVotes={votes}
-			comments={comments}
-			views={views}
-		/>
-	</ContainerLink>
-);
+	};
+
+	const startImpressionTimer = () => {
+		cancelImpressionTimer();
+		impressionTimeoutIdRef.current = setTimeout(() => {
+			sendImpressionByPostId(id);
+		}, 1500);
+	};
+
+	const onVisibilityChange = (isVisible) => {
+		if (isVisible) {
+			startImpressionTimer();
+		} else {
+			cancelImpressionTimer();
+		}
+	};
+
+	return (
+		<VisibilitySensor onChange={onVisibilityChange}>
+			<ContainerLink to={`post/${userPostId}`}>
+				{message ?
+					<UserPostEditor
+						measure={measure || (() => { })}
+						background={background || { type: 'none', id: -1 }}
+						editorInitialText={message}
+						isEditorReadOnly
+					/>
+					: null
+				}
+				{imageUrl ? <Image src={imageUrl} onLoad={measure || (() => { })} style={{ maxWidth: '400px' }} alt="" /> : null}
+				<FeedBottomBarFullStatistics
+					type={type}
+					date={date}
+					id={id}
+					userVote={vote}
+					totalVotes={votes}
+					comments={comments}
+					views={views}
+				/>
+			</ContainerLink>
+		</VisibilitySensor>
+	);
+};
 
 export default UserPost;
