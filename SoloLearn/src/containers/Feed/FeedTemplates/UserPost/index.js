58e0659e3e4ea154b484efc21ec663d59faf561d
Erik Davtyan <erikdavtyan@gmail.com>
2019-05-22 16:22:58 +0400

    Implement user post impressons

--- SoloLearn/src/containers/Feed/FeedTemplates/UserPost/index.js
+++ SoloLearn/src/containers/Feed/FeedTemplates/UserPost/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 import {
 	Container,
 	Image,
@@ -14,8 +15,12 @@ import {
 import { FeedBottomBarFullStatistics } from 'components/organisms';
 
 import UserPostEditor from 'containers/UserPostEditor/DraftEditor';
+import Service from 'api/service';
 
 import './styles.scss';
+
+const sendImpressionByPostId = id =>
+	Service.request(`Profile/AddPostImpression?id=${id}`);
 
 const UserPost = ({
 	user,
@@ -31,61 +36,88 @@ const UserPost = ({
 	userPostId,
 	comments,
 	views,
-}) => (
-	<FlexBox column className="user-post-feed-item-container">
-		<FlexBox align className="user-post-feed-item-profile-container">
-			<ProfileAvatar
-				user={user}
-			/>
-			<UsernameLink
-				to={`/profile/${user.id}`}
-				className="up-profile-username-link"
-			>
-				{user.name}
-			</UsernameLink>
-			<ModBadge
-				badge={user.badge}
-			/>
-		</FlexBox>
-		<ContainerLink to={`/post/${userPostId}`}>
-			{message ?
-				<Container style={{ padding: background ? 0 : '0 15px' }}>
-					<UserPostEditor
-						measure={measure || (() => { })}
-						background={background || { type: 'none', id: -1 }}
-						editorInitialText={
-							(message.match(/\n/g) || []).length > 5 ?
-								`${message.slice(0, 100)}`
-								:
-								message
-						}
-						isEditorReadOnly
+}) => {
+	const impressionTimeoutIdRef = useRef();
+
+	const cancelImpressionTimer = () => {
+		if (impressionTimeoutIdRef.current) {
+			clearTimeout(impressionTimeoutIdRef.current);
+		}
+	};
+
+	const startImpressionTimer = () => {
+		cancelImpressionTimer();
+		impressionTimeoutIdRef.current = setTimeout(() => {
+			sendImpressionByPostId(id);
+		}, 1500);
+	};
+
+	const onVisibilityChange = (isVisible) => {
+		if (isVisible) {
+			startImpressionTimer();
+		} else {
+			cancelImpressionTimer();
+		}
+	};
+
+	return (
+		<VisibilitySensor onChange={onVisibilityChange}>
+			<FlexBox column className="user-post-feed-item-container">
+				<FlexBox align className="user-post-feed-item-profile-container">
+					<ProfileAvatar
+						user={user}
 					/>
-					{(message.match(/\n/g) || []).length > 5 ?
-						<TextBlock className="up-feed-item-continue-reading-text">...Continue Reading</TextBlock> : null
+					<UsernameLink
+						to={`/profile/${user.id}`}
+						className="up-profile-username-link"
+					>
+						{user.name}
+					</UsernameLink>
+					<ModBadge
+						badge={user.badge}
+					/>
+				</FlexBox>
+				<ContainerLink to={`/post/${userPostId}`}>
+					{message ?
+						<Container style={{ padding: background ? 0 : '0 15px' }}>
+							<UserPostEditor
+								measure={measure || (() => { })}
+								background={background || { type: 'none', id: -1 }}
+								editorInitialText={
+									(message.match(/\n/g) || []).length > 5 ?
+										`${message.slice(0, 100)}`
+										:
+										message
+								}
+								isEditorReadOnly
+							/>
+							{(message.match(/\n/g) || []).length > 5 ?
+								<TextBlock className="up-feed-item-continue-reading-text">...Continue Reading</TextBlock> : null
+							}
+						</Container>
+						: null
 					}
-				</Container>
-				: null
-			}
-			{imageUrl ?
-				<Image
-					src={imageUrl}
-					onLoad={measure || (() => { })}
-					className="user-post-feed-image"
+					{imageUrl ?
+						<Image
+							src={imageUrl}
+							onLoad={measure || (() => { })}
+							className="user-post-feed-image"
+						/>
+						: null}
+				</ContainerLink>
+				<FeedBottomBarFullStatistics
+					type={type}
+					date={date}
+					id={id}
+					userVote={vote}
+					totalVotes={votes}
+					comments={comments}
+					views={views}
+					className="up-feed-item-bottom-bar"
 				/>
-				: null}
-		</ContainerLink>
-		<FeedBottomBarFullStatistics
-			type={type}
-			date={date}
-			id={id}
-			userVote={vote}
-			totalVotes={votes}
-			comments={comments}
-			views={views}
-			className="up-feed-item-bottom-bar"
-		/>
-	</FlexBox>
-);
+			</FlexBox>
+		</VisibilitySensor>
+	);
+};
 
 export default UserPost;
