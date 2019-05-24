93017069607fb78cd27f92adaeae8321046bcedc
Harutyun <har892.hh@hotmail.com>
2019-05-24 17:13:56 +0400

    disable mark all unread when no new notifications add nothing to show message

--- SoloLearn/src/containers/Notifications/components/Header/index.js
+++ SoloLearn/src/containers/Notifications/components/Header/index.js
@@ -4,21 +4,35 @@ import { translate } from 'react-i18next';
 import { Container, Title } from 'components/atoms';
 import { UsernameLink } from 'components/molecules';
 import { markRead } from 'actions/notifications';
+import { notificationsSelector } from 'reducers/notifications.reducer';
 
 import './styles.scss';
 
-const NotificationHeader = ({ t, markRead }) => (
-	<Container className="notifications-header">
-		<Title>{t('notifications.title')}</Title>
-		<UsernameLink
-			className="notifications_mark-read"
-			onClick={() => { markRead(null); }}
-		>
-			{t('notifications.mark-all-as-read-action-title')}
-		</UsernameLink>
-	</Container>
-);
+const isAllRead = (notifications) => {
+	console.log(notifications);
+	const isRead = notifications.find(not => not.isClicked === false);
+	console.log(isRead);
+	return isRead;
+};
+
+const NotificationHeader = ({ t, markRead, notifications }) => {
+	const isRead = isAllRead(notifications);
+
+	return (
+		<Container className="notifications-header">
+			<Title>{t('notifications.title')}</Title>
+			<UsernameLink
+				className={`notifications_mark-read ${!isRead && 'diasbale_mark-read'}`}
+				onClick={(e) => { isRead ? markRead(null) : e.preventDefault(); }}
+			>
+				{t('notifications.mark-all-as-read-action-title')}
+			</UsernameLink>
+		</Container>
+	);
+};
+
+const mapStateToProps = state =>	({ notifications: notificationsSelector(state) });
 
 const mapDispatchToProps = { markRead };
 
-export default translate()(connect(null, mapDispatchToProps)(NotificationHeader));
+export default translate()(connect(mapStateToProps, mapDispatchToProps)(NotificationHeader));
