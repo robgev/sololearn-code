97913bb2de293daa27740b6428cf9683988b53e0
Harutyun <har892.hh@hotmail.com>
2019-06-21 17:10:34 +0400

    search fug fixed

--- SoloLearn/src/containers/Discuss/Header/index.js
+++ SoloLearn/src/containers/Discuss/Header/index.js
@@ -14,19 +14,23 @@ import {
 import './styles.scss';
 import { Search, Add } from 'components/icons';
 
-const Header = ({ searchQuestion, onSearchChange }) => (
+const Header = ({
+	searchQuestion, onSearchChange, enterKeyPress, query,
+}) => (
 	<PaperContainer className="header-container">
 		<FlexBox fullWidth justifyBetween>
 			<Input
+				value={query}
 				onChange={onSearchChange}
+				onKeyDown={enterKeyPress}
 				fullWidth
 				className="search"
 				placeholder="Search for a question or answer"
 				variant="outlined"
 				InputProps={{
 					endAdornment: (
-						<InputAdornment position="end" className="search-icon">
-							<Search onClick={searchQuestion} />
+						<InputAdornment position="end" className="search-icon" onClick={searchQuestion}>
+							<Search />
 						</InputAdornment>
 					),
 					classes: { underline: 'adornmentEnd' },
