ed10a3776f24ccaaa56e73d17c487d15c871fe60
Robert Gevorgyan <robert1999.g@gmail.com>
2019-06-24 16:46:52 +0400

    Add small versions of several icons

---
+++ SoloLearn/src/components/icons/ViewsSmall.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ViewsSmall = props => (
+	<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill="currentColor" width="100%" height="100%" viewBox="0 0 22 22" {...props}>
+		<defs>
+			<clipPath id="clip-path">
+				<rect className="cls-1" width="22" height="22" />
+			</clipPath>
+		</defs>
+		<g id="ic_Impressions_small">
+			<rect id="Rectangle_359" data-name="Rectangle 359" width="22" height="22" fill="none" />
+			<g id="Group_512" data-name="Group 512" transform="translate(3 6)">
+				<path id="Path_608" data-name="Path 608" d="M1237.8,638.648a8.306,8.306,0,0,0-15.738,0l-.131.352.131.352a8.306,8.306,0,0,0,15.738,0l.131-.352ZM1229.932,643a4.3,4.3,0,1,1,5.871-4A6.424,6.424,0,0,1,1229.932,643Z" transform="translate(-1221.932 -633)" />
+				<circle id="Ellipse_146" data-name="Ellipse 146" cx="2" cy="2" r="2" transform="translate(6 4)" />
+			</g>
+		</g>
+	</svg>
+);
+
+export default ViewsSmall;
