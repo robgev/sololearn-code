0783f6be47d9e4daf27218fb951f0c4109674033
Sevak Ekizyan <sevakekizyan@gmail.com>
2019-07-25 15:18:45 +0400

    change comment date format

--- SoloLearn/src/utils/updateDate.js
+++ SoloLearn/src/utils/updateDate.js
@@ -3,6 +3,25 @@ import 'moment/locale/ru';
 import 'moment/locale/es';
 import i18n from 'i18n';
 
+moment.updateLocale('en', {
+	relativeTime: {
+		future: 'in %s',
+		past: '%s ago',
+		s: 'a few s',
+		ss: '%d s',
+		m: '1mmin',
+		mm: '%dmin',
+		h: '1h',
+		hh: '%dh',
+		d: '1d',
+		dd: '%dd',
+		M: '1mo',
+		MM: '%dmo',
+		y: '1y',
+		yy: '%dy',
+	},
+});
+
 const updateDate = (date) => {
 	moment.locale(i18n.language);
 	const postDate = moment.utc(date);
